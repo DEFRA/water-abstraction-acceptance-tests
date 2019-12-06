765cee7b12a51415ddf4d8d826d318697a5cbe98
Fayaz Mohammed <fayaz0211@gmail.com>
2019-12-06 15:22:10 +0000

    Added a base page class

---
+++ features/page_objects/common/base_page.rb
@@ -0,0 +1,18 @@
+class BasePage < SitePrism::Page
+
+  element(:h1_heading, ".govuk-heading-l")
+  element(:continue_button, ".govuk-button")
+
+
+  def click_continue
+    continue_button.click
+  end
+
+  def heading_title
+    h1_heading.text
+  end
+
+  def current_page_url
+    puts "The page url is: " + page.current_url
+  end
+end
