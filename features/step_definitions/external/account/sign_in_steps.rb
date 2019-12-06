1f41b43181af03be2a867850b8eaf4a43ff80cc5
Fayaz Mohammed <fayaz0211@gmail.com>
2019-12-06 16:44:05 +0000

    Added the step definition for internal user

--- features/step_definitions/external/account/sign_in_steps.rb
+++ features/step_definitions/external/account/sign_in_steps.rb
@@ -6,3 +6,14 @@ Given("I log in at the external test user") do
   user_name = data["externalPrimaryUser"]["user"]["user_name"]
   sign_in_page.submit_credentials(user_name, "P@55word")
 end
+
+Given(/^I logged in as ([^"]*) user$/) do |user|
+  sign_in_page = Pages::Internal::Account::SignIn.new
+  sign_in_page.load
+
+  data = @test_data.internal_users
+  user_name = data[user]["user_name"]
+  sign_in_page.submit_credentials(user_name, "P@55word")
+
+end
+
