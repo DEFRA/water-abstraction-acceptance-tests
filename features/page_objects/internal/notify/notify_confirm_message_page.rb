dcec9aec78115ceefb8fd4e060fa10c996bf24f6
Dana Gajewska <danusia.x@outlook.com>
2020-01-29 15:11:20 +0000

    Change css selectors to xpath

--- features/page_objects/internal/notify/notify_confirm_message_page.rb
+++ features/page_objects/internal/notify/notify_confirm_message_page.rb
@@ -1,9 +1,8 @@
 class NotifyConfirmMessagePage < SitePrism::Page
 
   element(:heading, "h1.govuk-heading-l")
-  # govuk-!-font-weight-bold is not a valid selector, even with escape character
-  element(:number_of_recipients, ".govuk-grid-column-two-thirds > p > span:nth-child(1)")
-  element(:number_of_licences, ".govuk-grid-column-two-thirds > p > span:nth-child(3)")
+  element(:number_of_recipients, :xpath, "//span[@class='govuk-!-font-weight-bold'][1]")
+  element(:number_of_licences, :xpath, "//span[@class='govuk-!-font-weight-bold'][2]")
   element(:message_preview, ".notification-preview")
   element(:continue_button, ".govuk-button")
 
