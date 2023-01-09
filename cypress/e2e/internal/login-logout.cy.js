f93647d6b693e5280227c9279ed656521d6dc6bb
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-01-09 13:55:05 +0000

    Migrate Login tests (#1)
    The existing tests have one that confirms login into the external app works. We migrate that along with adding one for the internal app. It made sense to us to have the same test for both apps. Plus it allows us to confirm all is working for both apps with our first test.

---
+++ cypress/e2e/internal/login-logout.cy.js
@@ -0,0 +1,31 @@
+'use strict'
+
+describe('Login and log out (internal)', () => {
+  before(() => {
+    cy.tearDown()
+    cy.setUp('barebones')
+    cy.fixture('users.json').its('billingAndData').as('userEmail')
+  })
+
+  it('can log in and out as an internal user', () => {
+    cy.visit('/')
+
+    //  Enter the user name and Password
+    cy.get('@userEmail').then((userEmail) => {
+      cy.get('input#email').type(userEmail)
+    })
+    cy.get('input#password').type(Cypress.env('defaultPassword'))
+
+    //  Click Sign in Button
+    cy.get('.govuk-button.govuk-button--start').click()
+
+    //  Assert the user signed in
+    cy.contains('Enter a licence number, customer name')
+
+    //  Click Sign out Button
+    cy.get('#signout').click()
+
+    //  Assert we are signed out
+    cy.contains("You're signed out").should('be.visible')
+  })
+})
