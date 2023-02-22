0257544bc47e939a9e99f04891d80157dcdb12e0
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-02-22 12:16:24 +0000

    Migrate user registration external test (#18)
    This migrates the external user registration test. When we reviewed what was there we found most of it duplicated validation that had already been checked elsewhere. It could also be argued that this testing is checked in `cypress/e2e/external/sharing-access-with-new-user.cy.js` but we've retained it to be on the safe side.
    
    What we have done is follow the pattern we used for **reset-password** where we split the validation tests from the journey test, to make what we're checking clearer.
    
    Whilst we were doing this, we realised we should really be doing the same for the **login**. 
    
    So, in this change, we also create a `login/` folder and move the existing `cypress/e2e/external/login-logout.cy.js` in there along with a new validation of the login page test.

---
+++ cypress/e2e/external/registration/journey.cy.js
@@ -0,0 +1,51 @@
+'use strict'
+
+describe('User registration (external)', () => {
+  before(() => {
+    cy.tearDown()
+    cy.setUp('barebones')
+    cy.fixture('users.json').its('externalNew').as('userEmail')
+  })
+
+  it('can register a new user', () => {
+    cy.visit(Cypress.env('externalUrl'))
+
+    // Tap the create account button on the welcome page
+    cy.get('a[href*="/start"]').click()
+
+    // Confirm we want to create an account
+    cy.contains('Create an account to manage your water abstraction licence online').should('have.class', 'govuk-heading-l')
+    cy.get('a[href*="/register"]').click()
+
+    //  Enter the email address submit
+    cy.get('@userEmail').then((userEmail) => {
+      cy.get('input#email').type(userEmail)
+    })
+    cy.get('button.govuk-button').click()
+
+    // Should be on the confirm your email page
+    cy.contains('Confirm your email address').should('have.class', 'govuk-heading-l')
+
+    cy.get('@userEmail').then((email) => {
+      cy.lastNotification(email).then((body) => {
+        cy.extractNotificationLink(body, 'link', Cypress.env('externalUrl')).then((link) => {
+          cy.visit(link)
+        })
+
+        cy.get('input#password').type(Cypress.env('defaultPassword'))
+        cy.get('input#confirmPassword').type(Cypress.env('defaultPassword'))
+        cy.get('form').submit()
+      })
+    })
+
+    //  Assert the user signed in
+    cy.url().should('include', '/add-licences')
+    cy.contains('Add your licences to the service').should('have.class', 'govuk-heading-l')
+
+    //  Click Sign out Button
+    cy.get('#signout').click()
+
+    //  Assert we are signed out
+    cy.contains("You're signed out").should('be.visible')
+  })
+})
