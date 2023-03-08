c72270b4c7e86c7794698cdff961d53eda167a0b
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-03-08 23:27:40 +0000

    Migrate view licences test (#22)
    This is the final external UI acceptance test to migrate.
    
    TBH, it is checking a page most of the other journeys go through. But we're migrating now to ensure there are no concerns with something being dropped by the wider team. We can review everything once it is all migrated.

---
+++ cypress/e2e/external/view-licences.cy.js
@@ -0,0 +1,31 @@
+'use strict'
+
+describe('View returns (external)', () => {
+  before(() => {
+    cy.tearDown()
+    cy.setUp('barebones')
+    cy.fixture('users.json').its('external').as('userEmail')
+  })
+
+  it('login as an existing user and view returns', () => {
+    cy.visit(Cypress.env('externalUrl'))
+
+    // tap the sign in button on the welcome page
+    cy.get('a[href*="/signin"]').click()
+
+    //  Enter the user name and Password
+    cy.get('@userEmail').then((userEmail) => {
+      cy.get('input#email').type(userEmail)
+    })
+    cy.get('input#password').type(Cypress.env('defaultPassword'))
+
+    //  Click Sign in Button
+    cy.get('.govuk-button.govuk-button--start').click()
+
+    // Confirm the licences are listed
+    cy.contains('AT/CURR/DAILY/01').should('be.visible')
+    cy.contains('AT/CURR/WEEKLY/01').should('be.visible')
+    cy.contains('AT/CURR/MONTHLY/01').should('be.visible')
+    cy.contains('AT/CURR/MONTHLY/02').should('be.visible')
+  })
+})
