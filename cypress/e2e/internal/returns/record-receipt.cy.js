a7d5676c9c03af97032dabb6acb3e63644de116e
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-06-03 11:21:30 +0100

    Migrate legacy Mark licence for supp. billing test
    We're onto the last folder of legacy tests; `internal/billing`. No surprises there are tests that have little to do with billing and would be better located elsewhere. The first of these is `mark-licence-for-supplementary-billing.spec.js`.
    
    It is in the billing folder and is named as a test to mark a licence for supplementary billing. But having review the test it primary concern is to submit a return against a licence. Marking the it for supplementary billing is a small step it tags on the end!
    
    So, we bring this in to our `internal/returns` folder. What we also realised was our existing `submit.cy.js` test was actually using the record receipt option in returns. So, a rename frees up 'submit' for the legacy test we're migrating.
    
    Finally, whilst we were doing this we knew that `internal/charge-information/sroc/journey.cy.js` also results in a licence being marked for supplementary billing. So, we tweaked the end of that test to include confirmation this happens.

---
+++ cypress/e2e/internal/returns/record-receipt.cy.js
@@ -0,0 +1,66 @@
+'use strict'
+
+describe('Record receipt for return (internal)', () => {
+  beforeEach(() => {
+    cy.tearDown()
+    cy.setUp('barebones')
+    cy.fixture('users.json').its('billingAndData').as('userEmail')
+  })
+
+  it('record the receipt for an overdue return for a licence from its returns tab', () => {
+    cy.visit('/')
+
+    // enter the user name and Password
+    cy.get('@userEmail').then((userEmail) => {
+      cy.get('input#email').type(userEmail)
+    })
+    cy.get('input#password').type(Cypress.env('defaultPassword'))
+
+    // click Sign in Button
+    cy.get('.govuk-button.govuk-button--start').click()
+
+    // assert the user signed in and we're on the search page
+    cy.contains('Search')
+
+    // search for a licence
+    cy.get('#query').type('AT/CURR/MONTHLY/02')
+    cy.get('.search__button').click()
+    cy.get('.govuk-table__row').contains('AT/CURR/MONTHLY/02').click()
+
+    // confirm we are on the licence page and select returns tab
+    cy.contains('AT/CURR/MONTHLY/02')
+    cy.get('#tab_returns').click()
+
+    // confirm we are on the tab page
+    cy.get('#returns > .govuk-heading-l').contains('Returns')
+
+    // confirm we see the overdue return
+    cy.get('section#returns > .govuk-table > .govuk-table__body').within(() => {
+      cy.get('.govuk-table__row:nth-child(5)').should('be.visible').and('contain.text', '9999990')
+      cy.get('.govuk-table__row:nth-child(5)').should('be.visible').and('contain.text', 'Overdue')
+
+      cy.get('.govuk-table__row:nth-child(5) a').contains('9999990').click()
+    })
+
+    // What do you want to do with this return?
+    // select record receipt
+    cy.get('#action-2').check()
+    cy.get('form > .govuk-button').click()
+
+    // When was the return received?
+    // leave defaulted current date
+    cy.get('form > .govuk-button').click()
+
+    // Return received
+    cy.get('.panel').contains('Return received Licence number AT/CURR/MONTHLY/02').should('be.visible')
+
+    // View returns for the licence (this is a different view)
+    cy.get('p > a').contains('View returns for AT/CURR/MONTHLY/02').should('be.visible').click()
+
+    // confirm we see the received return
+    cy.get('.govuk-table > .govuk-table__body').within(() => {
+      cy.get('.govuk-table__row:nth-child(5)').should('be.visible').and('contain.text', 'January 2019 to December 2019')
+      cy.get('.govuk-table__row:nth-child(5)').should('be.visible').and('contain.text', 'Received')
+    })
+  })
+})
