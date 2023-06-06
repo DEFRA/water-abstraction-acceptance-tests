bf9bd14e9f21f4495aab261f34abd63e1066483f
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-06-06 23:11:42 +0100

    Migrate legacy non-chargeable licence credits (#49)
    This is the last of the tests (!!) we found in `integration/internal/billing` that needed to be migrated. It is a test that checks if a licence is made non-chargeable (which is done by adding special charge information) then a credit is generated in the next supplementary bill run.
    
    It requires us to first create, confirm and send a supplementary bill run, then add the charge information, and then create another supplementary bill run.
    
    As such we struggled to determine where exactly it fitted within our current folder structure. We opted for a new one; `internal/billing/scenarios`. We know we will be adding more SROC billing scenarios in the very near future so it made sense to give them and this migrated test a home.
    
    This change came in after support was added for multiple years to the SROC billing engine. We know it's broken some of our tests but we wanted to make sure this one was passing before merging. So, to support that and fixing the others we needed to be dynamic with what date to use in some of our screens. This change also adds a new custom command `currentFinancialYearDate()` which allows us to generate dates in the current financial year for use in our tests.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -137,3 +137,41 @@ Cypress.Commands.add('reloadUntilTextFound', (selector, textToMatch, retries = 3
   cy.reload()
   cy.reloadUntilTextFound(selector, textToMatch, retries - 1, retryWait)
 })
+
+// We do not control when the tests are run so sometimes we need a date that is within the current financial year when
+// they are. For example, when testing billing scenarios we often only want to make charge information changes within
+// the current year to avoid additional calculations for previous years.
+//
+// It defaults to the last possible date. If the current date was 2023-06-05 it would return 2024-03-31. You can
+// override the day and month (don't worry and month being zero-indexed - it gets dealt with!) and adjust the year
+// by plus or minus as many years as you need.
+Cypress.Commands.add('currentFinancialYearDate', (day = 31, month = 3, yearAdjuster = 0) => {
+  // IMPORTANT! getMonth returns an integer (0-11). So, January is represented as 0 and December as 11. This is why
+  // MARCH is 2 rather than 3
+  const MARCH = 2
+
+  const currentDate = new Date()
+  const currentYear = currentDate.getFullYear()
+
+  let endYear
+
+  if (currentDate.getMonth() <= MARCH) {
+    // For example, if currentDate was 2022-02-15 it would fall in financial year 2021-04-01 to 2022-03-31
+    endYear = currentYear + yearAdjuster
+  } else {
+    // For example, if currentDate was 2022-06-15 it would fall in financial year 2022-04-01 to 2023-03-31
+    endYear = (currentYear + 1) + yearAdjuster
+  }
+
+  // we provide the result as this so callers of this function can choose to use the date value or access the
+  // individual elements for use in input fields
+  const result = {
+    date: new Date(Date.UTC(endYear, day, month - 1)),
+    day,
+    month,
+    year: endYear
+  }
+
+  // We generate the date value using Date.UTC() to avoid 31 March becoming 30 March 23:00 because of pesky BST
+  return cy.wrap(result)
+})
