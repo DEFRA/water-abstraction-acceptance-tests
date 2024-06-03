5d07b742d6e241ce24615567b135dee84b776001
Roble <roblesiyad123@gmail.com>
2024-06-03 16:08:39 +0100

    Adding more acceptance tests for return requirements journey

--- cypress/e2e/internal/returns-requirements/returns-required.cy.js
+++ cypress/e2e/internal/returns-requirements/returns-required.cy.js
@@ -34,5 +34,31 @@ describe('Submit and cancel no returns requirement (internal)', () => {
 
     // confirm we are on the charge information tab
     cy.get('#charge > :nth-child(1)').contains('Charge information')
+
+    // click set up new returns requirement
+    cy.get('[data-test="meta-data-returns-required"]').click()
+
+    // choose the licence version start date and click continue
+    cy.get('#licence-start-date').check()
+    cy.get('.govuk-button').click()
+
+    // confirm we are on the reason page
+    cy.get('.govuk-fieldset__heading').contains('Select the reason for the requirements for returns')
+
+    // choose returns exception and click continue
+    cy.get('#reason-2').check()
+    cy.get('.govuk-button').click()
+
+    // confirm we are on the set up page
+    cy.get('.govuk-fieldset__heading').contains('How do you want to set up the requirements for returns?')
+
+    // click set up manually and continue
+    cy.get('#setup-3').check()
+    cy.get('.govuk-button').click()
+
+    // confirm we are on the purpose page
+    cy.get('.govuk-heading-xl').contains('Select the purpose for the requirements for returns')
+
+    // choose a purpose for the requirement
   })
 })
