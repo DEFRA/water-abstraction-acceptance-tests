bb5e1c6fa1fbd9cc44167f8089f421b428656e2e
Roble <roblesiyad123@gmail.com>
2024-06-05 12:40:12 +0100

    Add tests for changing values within the requirements journey and reconfirming values

--- cypress/e2e/internal/returns-requirements/returns-required.cy.js
+++ cypress/e2e/internal/returns-requirements/returns-required.cy.js
@@ -122,9 +122,10 @@ describe('Submit and cancel no returns requirement (internal)', () => {
     // confirm we are on the check page
     cy.get('.govuk-heading-xl').contains('Check the return requirements for Mr J J Testerson')
 
-    // confirm we can see the information we have selected and make changes
-    // start date
+    // confirm we see the start date information we expect
     cy.get('#main-content > :nth-child(2)').contains('1 April 2022')
+
+    // choose the change option for the start date
     cy.get('[data-test="meta-data-change-start-date"]').click()
 
     // change start date and continue
@@ -134,30 +135,147 @@ describe('Submit and cancel no returns requirement (internal)', () => {
     cy.get('#other-start-date-year').type('2023')
     cy.get('.govuk-button').click()
 
-    // confirm we are back on check page and start date changes
-    cy.get('#main-content > :nth-child(2)').contains('2 August 2023')
+    // confirm we are back on check page and see the start date changes
+    cy.get('.govuk-heading-xl').contains('Check the return requirements for Mr J J Testerson')
+    cy.get('[data-test="meta-data-start-date"]').contains('2 August 2023')
 
-    // // reason
-    // cy.get('#main-content > :nth-child(2)').contains('Licence holder name or address change')
+    // confirm we see the reason we selected
+    cy.get('[data-test="meta-data-reason"]').contains('Licence holder name or address change')
 
-    // // purposes
+    // choose the change option for reason
+    cy.get('[data-test="meta-data-change-reason"]').click()
 
-    // // points
+    // change the reason and continue
+    cy.get('#reason-8').check()
+    cy.get('.govuk-button').click()
 
-    // // abstraction period
-    // cy.get(':nth-child(3) > .govuk-summary-list__value').contains('From 1 December to 3 September')
+    // confirm we are back on the check page and see the reason changes
+    cy.get('.govuk-heading-xl').contains('Check the return requirements for Mr J J Testerson')
+    cy.get('[data-test="meta-data-reason"]').contains('New licence')
 
-    // // returns cycle
+    // confirm we see the purposes selected
+    cy.get('.govuk-summary-card__content').contains('requirement.purposes')
 
-    // // site description
-    // cy.get(':nth-child(5) > .govuk-summary-list__value').contains('This is a valid site description')
+    // choose the change option for purposes
+    cy.get('[data-test="meta-data-change-purpose"]').click()
 
-    // // collection frequency
-    // cy.get(':nth-child(6) > .govuk-summary-list__value').contains('Daily')
+    // change the purpose and continue
+    cy.get('#purposes').uncheck()
+    cy.get('#purposes-2').check()
+    cy.get('.govuk-button').click()
 
-    // // reporting frequency
-    // cy.get(':nth-child(7) > .govuk-summary-list__value').contains('Daily')
+    // confirm we are back on the check page and see the purpose changes
+    cy.get('.govuk-heading-xl').contains('Check the return requirements for Mr J J Testerson')
+    cy.get('.govuk-summary-card__content').contains('requirement.purposes')
 
-    // // agreements and exceptions
+    // confirm we see the points selected
+    cy.get('.govuk-summary-card__content').contains('requirement.points')
+
+    // choose the change option for points
+    cy.get('[data-test="meta-data-change-points"]').click()
+
+    // change the points and continue
+    cy.get('#points').uncheck()
+    cy.get('#points-2').check()
+    cy.get('.govuk-button').click()
+
+    // confirm we are back on the check page and see the points changes
+    cy.get('.govuk-heading-xl').contains('Check the return requirements for Mr J J Testerson')
+    cy.get('.govuk-summary-card__content').contains('requirement.points')
+
+    // confirm we see the abstraction period selected
+    cy.get(':nth-child(3) > .govuk-summary-list__value').contains('From 1 December to 3 September')
+
+    // choose the change option for the abstraction period
+    cy.get('[data-test="meta-data-change-abstraction-period"]').click()
+
+    // change the abstraction period and continue
+    cy.get('#abstraction-period-start-day').clear()
+    cy.get('#abstraction-period-start-month').clear()
+    cy.get('#abstraction-period-end-day').clear()
+    cy.get('#abstraction-period-end-month').clear()
+
+    cy.get('#abstraction-period-start-day').type('02')
+    cy.get('#abstraction-period-start-month').type('10')
+    cy.get('#abstraction-period-end-day').type('05')
+    cy.get('#abstraction-period-end-month').type('12')
+    cy.get('.govuk-button').click()
+
+    // confirm we are back on the check page and see the abstraction period changes
+    cy.get('.govuk-heading-xl').contains('Check the return requirements for Mr J J Testerson')
+    cy.get(':nth-child(3) > .govuk-summary-list__value').contains('From 2 October to 5 December')
+
+    // confirm we see the returns cycle selected
+    cy.get(':nth-child(4) > .govuk-summary-list__value').contains('requirement.returnsCycle')
+
+    // choose the change option for the returns cycle
+    cy.get('[data-test="meta-data-change-returns-cycle"]').click()
+
+    // change the returns cycle and continue
+    cy.get('#returnsCycle-2').check()
+    cy.get('.govuk-button').click()
+
+    // confirm we are back on the check page and see the returns cycle changes
+    cy.get('.govuk-heading-xl').contains('Check the return requirements for Mr J J Testerson')
+    cy.get(':nth-child(4) > .govuk-summary-list__value').contains('requirement.returnsCycle')
+
+    // confirm we see the site description we selected
+    cy.get(':nth-child(5) > .govuk-summary-list__value').contains('This is a valid site description')
+
+    // choose the change option for the site description
+    cy.get('[data-test="meta-data-change-site-description"]').click()
+
+    // change the site description and continue
+    cy.get('#site-description').clear()
+    cy.get('#site-description').type('This is another valid site description')
+    cy.get('.govuk-button').click()
+
+    // confirm we are back on the check page and see the site description changes
+    cy.get('.govuk-heading-xl').contains('Check the return requirements for Mr J J Testerson')
+    cy.get(':nth-child(5) > .govuk-summary-list__value').contains('This is another valid site description')
+
+    // confirm we see the collection frequency we selected
+    cy.get(':nth-child(6) > .govuk-summary-list__value').contains('Daily')
+
+    // choose the change option for the collection frequency
+    cy.get('[data-test="meta-data-change-frequency-collected"]').click()
+
+    // change the collection frequency and continue
+    cy.get('#frequencyCollected-2').check()
+    cy.get('.govuk-button').click()
+
+    // confirm we are back on the check page and see the collection frequency changes
+    cy.get('.govuk-heading-xl').contains('Check the return requirements for Mr J J Testerson')
+    cy.get(':nth-child(6) > .govuk-summary-list__value').contains('Weekly')
+
+    // confirm we see the reporting frequency we selected
+    cy.get(':nth-child(7) > .govuk-summary-list__value').contains('Daily')
+
+    // choose the change option for the reporting frequency
+    cy.get('[data-test="meta-data-change-frequency-reported"]').click()
+
+    // change the reporting frequency and continue
+    cy.get('#frequencyReported-3').check()
+    cy.get('.govuk-button').click()
+
+    // confirm we are back on the check page and see the collection frequency changes
+    cy.get('.govuk-heading-xl').contains('Check the return requirements for Mr J J Testerson')
+    cy.get(':nth-child(7) > .govuk-summary-list__value').contains('Monthly')
+
+    // confirm we see the agreements and exceptions we selected
+    cy.get(':nth-child(8) > .govuk-summary-list__value').contains('requirement.agreementsExceptions')
+
+    // choose the change option for agreements exceptions
+    cy.get('[data-test="meta-data-change-agreements-exceptions"]').click()
+
+    // change the agreements exceptions and continue
+    cy.get('#agreementsExceptions').uncheck()
+    cy.get('#agreementsExceptions-2').check()
+    cy.get('#agreementsExceptions-3').check()
+    cy.get('.govuk-button').click()
+
+    // confirm we are back on the check page and see the agreements exceptions changes
+    cy.get('.govuk-heading-xl').contains('Check the return requirements for Mr J J Testerson')
+    cy.get(':nth-child(8) > .govuk-summary-list__value').contains('requirement.agreementsExceptions')
   })
 })
