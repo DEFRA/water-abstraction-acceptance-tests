9efca271e40ea200e12d239bb623c845bbfe04c9
Becky Ransome <91424856+Beckyrose200@users.noreply.github.com>
2024-06-06 12:12:12 +0100

    Scenario one for the two-part tariff review pages (#92)
    https://eaflood.atlassian.net/browse/WATER-4452
    
    As the work for the two-part tariff engine and review pages is nearing completion, we are starting to create automated acceptance tests for the different scenarios we mapped out at the beginning of this work.
    
    This PR focuses on two testing scenarios. The first is an empty bill run. This is testing the bill run being created, it is empty and then deleting the bill run. The second is called 'Scenario one' as per the scenario spreadsheet we used during the creation of our two-part tariff engine. This scenario is the simplest happy-path test scenario we can think of.
    The test is setting up a new two-part tariff bill run with the data from 'scenario one', and then checking the review pages are displayed as expected.

---
+++ cypress/e2e/internal/billing/two-part-tariff/empty-bill-run.cy.js
@@ -0,0 +1,74 @@
+'use strict'
+
+describe('Create a empty SROC two-part tariff bill run (internal)', () => {
+  beforeEach(() => {
+    cy.tearDown()
+
+    cy.fixture('users.json').its('billingAndData1').as('userEmail')
+
+    cy.fixture('sroc-two-part-tariff.json').then((fixture) => {
+      cy.load(fixture)
+    })
+
+    // Get the current date as a string, for example 12 July 2023
+    cy.dayMonthYearFormattedDate().then((formattedCurrentDate) => {
+      cy.wrap(formattedCurrentDate).as('formattedCurrentDate')
+    })
+  })
+
+  it('creates an empty SROC two-part tariff bill run', () => {
+    cy.visit('/')
+
+    // Enter the user name and password
+    cy.get('@userEmail').then((userEmail) => {
+      cy.get('input#email').type(userEmail)
+    })
+
+    cy.get('input#password').type(Cypress.env('defaultPassword'))
+
+    // Click the Sign in Button
+    cy.get('.govuk-button.govuk-button--start').click()
+
+    //  Assert the user signed in and we're on the search page
+    cy.contains('Search')
+
+    // click the Bill runs menu link
+    cy.get('#navbar-bill-runs').contains('Bill runs').click()
+
+    // Bill runs
+    // click the Create a bill run button
+    cy.get('.govuk-button').contains('Create a bill run').click()
+
+    // Which kind of bill run do you want to create?
+    // choose Two-part tariff then continue
+    cy.get('label.govuk-radios__label').contains('Two-part tariff').click()
+    cy.get('form > .govuk-button').contains('Continue').click()
+
+    // Select the region
+    // choose Test Region and continue
+    cy.get('label.govuk-radios__label').contains('Test Region').click()
+    cy.get('form > .govuk-button').contains('Continue').click()
+
+    // Select the financial year
+    // choose 2021 to 2022 and continue
+    cy.get('label.govuk-radios__label').contains('2023 to 2024').click()
+    cy.get('form > .govuk-button').contains('Continue').click()
+
+    // The bill run we created will be the top result. We expect it's status to be BUILDING. Building might take a few
+    // seconds though so to avoid the test failing we use our custom Cypress command to look for the status EMPTY, and
+    // if not found reload the page and try again. We then select it using the link on the date created
+    cy.reloadUntilTextFound('[data-test="bill-run-status-0"] > .govuk-tag', 'empty')
+    cy.get('@formattedCurrentDate').then((formattedCurrentDate) => {
+      cy.get('[data-test="date-created-0"]').should('contain.text', formattedCurrentDate)
+    })
+    cy.get('[data-test="region-0"]').should('contain.text', 'Test Region')
+    cy.get('[data-test="bill-run-type-0"]').should('contain.text', 'Two-part tariff')
+    cy.get('[data-test="date-created-0"] > .govuk-link').click()
+
+    // Test Region two-part tariff bill run
+    // quick test that the display is as expected and then click Cancel bill run
+    cy.get('.govuk-body > .govuk-tag').should('contain.text', 'empty')
+    cy.get('[data-test="error-notification"]').should('exist')
+    cy.get('[data-test="error-notification"]').should('contain.text', 'There are no licences ready for this bill run')
+  })
+})
