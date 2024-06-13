683e616a1d65f83ac56195d4b64e26b3ffd9b9f3
Roble <roblesiyad123@gmail.com>
2024-06-13 13:42:07 +0100

    WIP copy existing

---
+++ cypress/e2e/internal/returns-requirements/copy-existing.cy.js
@@ -0,0 +1,59 @@
+'use strict'
+
+describe('Submit returns requirement using copy existing (internal)', () => {
+  beforeEach(() => {
+    cy.tearDown()
+
+    cy.fixture('returns-requirements.json').then((fixture) => {
+      cy.load(fixture)
+    })
+
+    cy.fixture('users.json').its('billingAndData1').as('userEmail')
+  })
+
+  it('creates a return requirement by copying existing and approves the requirement', () => {
+    cy.visit('/')
+
+    // enter the user name and Password
+    cy.get('@userEmail').then((userEmail) => {
+      cy.get('#email').type(userEmail)
+    })
+
+    cy.get('#password').type(Cypress.env('defaultPassword'))
+
+    // click Sign in Button
+    cy.get('form > .govuk-button').click()
+
+    // assert the user signed in and we're on the search page
+    cy.contains('Search')
+
+    // search for a licence
+    cy.get('#query').type('AT/TEST/01')
+    cy.get('.search__button').click()
+    cy.get('.govuk-table__row > :nth-child(1) > a').click()
+
+    // confirm we are on the licence page and select charge information tab
+    cy.contains('AT/TEST/01')
+    cy.get('#tab_charge').click()
+
+    // confirm we are on the charge information tab
+    cy.get('#charge > :nth-child(1)').contains('Charge information')
+
+    // click set up new returns requirement
+    cy.contains('Set up new returns requirement').click()
+
+    // choose the licence version start date and click continue
+    cy.get('#licence-start-date').check()
+    cy.contains('Continue').click()
+
+    // confirm we are on the reason page
+    cy.get('.govuk-fieldset__heading').contains('Select the reason for the requirements for returns')
+
+    // choose returns exception and click continue
+    cy.get('#reason-2').check()
+    cy.contains('Continue').click()
+
+    // confirm we are on the set up page
+    cy.get('.govuk-fieldset__heading').contains('How do you want to set up the requirements for returns?')
+  })
+})
