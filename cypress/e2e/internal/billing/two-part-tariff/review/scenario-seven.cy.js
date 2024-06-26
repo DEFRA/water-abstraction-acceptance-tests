7cc10a1564f48fdad6cda70cef84136f9f06c0fc
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-06-26 12:29:28 +0100

    Restructure scenario 7

--- cypress/e2e/internal/billing/two-part-tariff/review/scenario-seven.cy.js
+++ cypress/e2e/internal/billing/two-part-tariff/review/scenario-seven.cy.js
@@ -3,12 +3,15 @@
 describe('Testing a two-part tariff bill run with a similar licence to scenario one, licence is current and not in workflow, it has one applicable charge version with a single charge reference and one charge element. It has one return with a status of "received"', () => {
   beforeEach(() => {
     cy.tearDown()
-    cy.fixture('sroc-two-part-tariff-simple-licence-data.json').then((fixture) => {
-      // We set the status to "received" to flag the issue
-      fixture.returnLogs[0].status = 'received'
-
+    // Load the base licence information into the DB
+    cy.fixture('review-scenario-licence.json').then((fixture) => {
       cy.load(fixture)
     })
+    // Load the charge and returns information into the DB
+    cy.fixture('review-scenario-seven.json').then((fixture) => {
+      cy.load(fixture)
+    })
+    // Grab the user email to use
     cy.fixture('users.json').its('billingAndData1').as('userEmail')
 
     // Get the current date as a string, for example 12 July 2023
