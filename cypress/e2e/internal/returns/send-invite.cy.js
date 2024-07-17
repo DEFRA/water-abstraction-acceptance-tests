36d232d9b4efcdc2e677073a3831e5ace51b8835
Rebecca Ransome <beckyrose200@aol.com>
2024-07-17 19:03:46 +0100

    send invite using barebones

--- cypress/e2e/internal/returns/send-invite.cy.js
+++ cypress/e2e/internal/returns/send-invite.cy.js
@@ -3,7 +3,9 @@
 describe('Send returns invite to customer (internal)', () => {
   beforeEach(() => {
     cy.tearDown()
-    cy.setUp('barebones')
+    cy.fixture('barebones.json').then((fixture) => {
+      cy.load(fixture)
+    })
     cy.fixture('users.json').its('billingAndData').as('userEmail')
   })
 
