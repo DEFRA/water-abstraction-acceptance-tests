3be7eca4a8601265d849f7e746a08c6880b34468
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-09-08 22:18:14 +0100

    Update rtn requirement fixtures to use new points (#134)
    https://eaflood.atlassian.net/browse/WATER-4600
    https://eaflood.atlassian.net/browse/WATER-4645
    
    > Part of the work to migrate return versions from NALD to WRLS
    
    We've been extending and amending the import of return versions from NALD to WRLS as part of our work to switch from NALD to WRLS to manage them. To support this, we [Created a return-requirement-points table](https://github.com/DEFRA/water-abstraction-service/pull/2540) and [updated the import](https://github.com/DEFRA/water-abstraction-import/pull/933) to import these point details.
    
    Users select these points as part of the return requirements setup journey we've built. We were extracting them from the JSON blob stored in the `permit.licence` table. However, the import service only populates the points for licences.
    
    - That have not ended
    - That have a current licence version
    
    Otherwise, `permit.licence` is not populated with the points data our journey relies on, causing it to throw an error.
    
    So, we needed another source for points. In [Start importing licence version points to 'water'](https://github.com/DEFRA/water-abstraction-import/pull/1009) we updated the **water-abstraction-import** to start importing licence version purpose points to a new table. Then, in [Update Return req set up journey to use new points](https://github.com/DEFRA/water-abstraction-system/pull/1301), we updated **water-abstraction-system** to use this new source in the journey.
    
    These changes have fixed the problem but broken our acceptance tests! We must load the new `water.licence_version_purpose_points` table instead of the `permit.licence` table.
    
    In this change, we update the fixtures that depend on populating the `permit.licence` table to populate our new table instead.

--- cypress/e2e/internal/returns-requirements/returns-required.cy.js
+++ cypress/e2e/internal/returns-requirements/returns-required.cy.js
@@ -155,7 +155,7 @@ describe('Submit returns requirement (internal)', () => {
     cy.get('[data-test="reason"]').contains('Minor change')
 
     // confirm we see the purposes selected
-    cy.get('[data-test="purposes-0"]').should('contain', 'General Farming & Domestic (This is a purpose description)')
+    cy.get('[data-test="purposes-0"]').should('contain', 'Hydroelectric Power Generation (This is a purpose description)')
 
     // choose the change option for purposes
     cy.get('[data-test="change-purposes-0"]').click()
@@ -168,10 +168,10 @@ describe('Submit returns requirement (internal)', () => {
 
     // confirm we are back on the check page and see the purpose changes
     cy.get('.govuk-heading-xl').contains('Check the requirements for returns for Mr J J Testerson')
-    cy.get('[data-test="purposes-0"]').contains('Hydroelectric Power Generation (This is another purpose description)')
+    cy.get('[data-test="purposes-0"]').contains('Laundry Use (This is another purpose description)')
 
     // confirm we see the points selected
-    cy.get('[data-test="points-0"]').should('contain', 'At National Grid Reference TQ 1234 1234 (Test local name 1)')
+    cy.get('[data-test="points-0"]').should('contain', 'At National Grid Reference TQ 1234 5678 (Example licence point 1)')
 
     // choose the change option for points
     cy.get('[data-test="change-points-0"]').click()
@@ -183,7 +183,7 @@ describe('Submit returns requirement (internal)', () => {
 
     // confirm we are back on the check page and see the points changes
     cy.get('.govuk-heading-xl').contains('Check the requirements for returns for Mr J J Testerson')
-    cy.get('[data-test="points-0"]').contains('At National Grid Reference TT 5678 5678 (Test local name 2)')
+    cy.get('[data-test="points-0"]').contains('At National Grid Reference TT 9876 5432 (Example licence point 2)')
 
     // confirm we see the abstraction period selected
     cy.get('[data-test="abstraction-period-0"]').contains('From 1 December to 3 September')
---
+++ cypress/e2e/internal/returns-requirements/start-by-using-abstraction-data.cy.js
@@ -0,0 +1,105 @@
+'use strict'
+
+describe('Submit returns requirement (internal) using abstraction data', () => {
+  beforeEach(() => {
+    cy.tearDown()
+
+    cy.fixture('returns-requirements.json').then((fixture) => {
+      cy.load(fixture)
+    })
+
+    cy.fixture('users.json').its('billingAndData').as('userEmail')
+  })
+
+  it('creates a return requirement using abstraction data and approves the requirement', () => {
+    cy.visit('/')
+
+    // enter the user name and Password
+    cy.get('@userEmail').then((userEmail) => {
+      cy.get('#email').type(userEmail)
+    })
+
+    cy.get('#password').type(Cypress.env('defaultPassword'))
+
+    // click Sign in Button
+    cy.get('form > .govuk-button').click()
+
+    // assert the user signed in and we're on the search page
+    cy.contains('Search')
+
+    // search for a licence
+    cy.get('#query').type('AT/TEST/01')
+    cy.get('.search__button').click()
+    cy.get('.govuk-table__row > :nth-child(1) > a').click()
+
+    // confirm we are on the licence page and select licence set up tab
+    cy.contains('AT/TEST/01')
+    cy.contains('Licence set up').click()
+
+    // confirm we are on the licence set up tab
+    cy.get('#set-up > .govuk-heading-l').contains('Licence set up')
+
+    // click set up new requirements
+    cy.contains('Set up new requirements').click()
+
+    // choose the licence version start date and click continue
+    cy.get('#licence-start-date').check()
+    cy.contains('Continue').click()
+
+    // confirm we are on the reason page
+    cy.get('.govuk-fieldset__heading').contains('Select the reason for the requirements for returns')
+
+    // choose reason (new licence) and click continue
+    cy.get('#reason-10').check()
+    cy.contains('Continue').click()
+
+    // confirm we are on the set up page
+    cy.get('.govuk-fieldset__heading').contains('How do you want to set up the requirements for returns?')
+
+    // choose the start by using abstraction data checkbox and continue
+    cy.get('#setup').check()
+    cy.contains('Continue').click()
+
+    // confirm we are back on the check page
+    cy.get('.govuk-heading-xl').contains('Check the requirements for returns for Mr J J Testerson')
+
+    // confirm we see the start data and reason options selected previously
+    cy.get('[data-test="start-date"]').contains('12 June 2023')
+    cy.get('[data-test="reason"]').contains('New licence')
+
+    // confirm we see return requirements generated from abstraction data
+    // Return requirement 1
+    cy.get('#requirement-0 > div.govuk-summary-card__title-wrapper > h2').contains('Example licence point 1')
+    cy.get('[data-test="purposes-0"]').contains('Hydroelectric Power Generation')
+    cy.get('[data-test="points-0"]').contains('At National Grid Reference TQ 1234 5678 (Example licence point 1)')
+    cy.get('[data-test="abstraction-period-0"]').contains('From 1 March to 31 December')
+    cy.get('[data-test="returns-cycle-0"]').contains('Winter and all year')
+    cy.get('[data-test="site-description-0"]').contains('Example licence point 1')
+    cy.get('[data-test="frequency-collected-0"]').contains('Daily')
+    cy.get('[data-test="frequency-reported-0"]').contains('Daily')
+    cy.get('[data-test="agreements-exceptions-0"]').contains('None')
+
+    // Return requirement 2
+    cy.get('#requirement-1 > div.govuk-summary-card__title-wrapper > h2').contains('Example licence point 2')
+    cy.get('[data-test="purposes-1"]').contains('Laundry Use')
+    cy.get('[data-test="points-1"]').contains('At National Grid Reference TT 9876 5432 (Example licence point 2)')
+    cy.get('[data-test="abstraction-period-1"]').contains('From 12 June to 29 November')
+    cy.get('[data-test="returns-cycle-1"]').contains('Winter and all year')
+    cy.get('[data-test="site-description-1"]').contains('Example licence point 2')
+    cy.get('[data-test="frequency-collected-1"]').contains('Monthly')
+    cy.get('[data-test="frequency-reported-1"]').contains('Monthly')
+    cy.get('[data-test="agreements-exceptions-1"]').contains('None')
+
+    // choose the approve return requirement button
+    cy.contains('Approve returns requirement').click()
+
+    // confirm we are on the approved page
+    cy.get('.govuk-panel__title').contains('Requirements for returns approved')
+
+    // click link to return to licence set up
+    cy.contains('Return to licence set up').click()
+
+    // confirm we are on the licence set up tab
+    cy.get('#set-up > .govuk-heading-l').contains('Licence set up')
+  })
+})
