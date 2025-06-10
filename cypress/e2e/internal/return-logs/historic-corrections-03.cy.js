f545ada19f3a9050fd81d2ef9a4840fc42b43a62
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-06-10 11:50:47 +0100

    Update return log tests after reference change
    https://eaflood.atlassian.net/browse/WATER-5085
    
    An issue was found with the way we were generating the 'legacy_id' (return reference) for new return requirements. We had incorrectly assumed that they were like most NALD records: unique within a region only.
    
    So, our logic would determine the current largest reference within a region, add 1 to it, and use that for the new reference.
    
    Turns out since 2008 they have been unique across _all_ regions. So now we have an auto-increment on the table itself. When new records are added PostgreSQL just sets the reference to the next increment.
    
    The issue for the tests is, we are assigning a legacy ID that is in the 999999* range to avoid clashing with real return requirements. But now the reference is determined by the DB, it will just look at what its next increment will be. At time of writing, for all environments that is in the 10****** range.
    
    When it comes to the historic return log tests, they are still seeing the expected return logs created, its just they are now in a different order because of the change in references.

--- cypress/e2e/internal/return-logs/historic-corrections-03.cy.js
+++ cypress/e2e/internal/return-logs/historic-corrections-03.cy.js
@@ -173,45 +173,50 @@ describe('Submit historic correction using abstraction data for two abstraction 
 
       cy.returnLogDueData(endYear, true).then((data) => {
         cy.get('[data-test="return-due-date-0"]').contains(data.text)
-        cy.get('[data-test="return-status-0"] > .govuk-tag').contains(data.label)
+        cy.get('[data-test="return-status-0"] > .govuk-tag').contains('void')
 
         cy.get('[data-test="return-due-date-1"]').contains(data.text)
-        cy.get('[data-test="return-status-1"] > .govuk-tag').contains(data.label)
+        cy.get('[data-test="return-status-1"] > .govuk-tag').contains('void')
 
         cy.get('[data-test="return-due-date-2"]').contains(data.text)
-        cy.get('[data-test="return-status-2"] > .govuk-tag').contains('void')
+        cy.get('[data-test="return-status-2"] > .govuk-tag').contains(data.label)
 
         cy.get('[data-test="return-due-date-3"]').contains(data.text)
-        cy.get('[data-test="return-status-3"] > .govuk-tag').contains('void')
+        cy.get('[data-test="return-status-3"] > .govuk-tag').contains(data.label)
       })
 
       cy.returnLogDueData(startYear, true).then((data) => {
         cy.get('[data-test="return-due-date-4"]').contains(data.text)
-        cy.get('[data-test="return-status-4"] > .govuk-tag').contains(data.label)
+        cy.get('[data-test="return-status-4"] > .govuk-tag').contains('void')
 
         cy.get('[data-test="return-due-date-5"]').contains(data.text)
-        cy.get('[data-test="return-status-5"] > .govuk-tag').contains(data.label)
+        cy.get('[data-test="return-status-5"] > .govuk-tag').contains('void')
 
         cy.get('[data-test="return-due-date-6"]').contains(data.text)
-        cy.get('[data-test="return-status-6"] > .govuk-tag').contains('void')
+        cy.get('[data-test="return-status-6"] > .govuk-tag').contains(data.label)
 
         cy.get('[data-test="return-due-date-7"]').contains(data.text)
-        cy.get('[data-test="return-status-7"] > .govuk-tag').contains('void')
+        cy.get('[data-test="return-status-7"] > .govuk-tag').contains(data.label)
       })
 
       cy.returnLogDueData(startYear - 1, true).then((data) => {
         cy.get('[data-test="return-due-date-8"]').contains(data.text)
-        cy.get('[data-test="return-status-8"] > .govuk-tag').contains(data.label)
+        cy.get('[data-test="return-status-8"] > .govuk-tag').contains('void')
+
         cy.get('[data-test="return-due-date-9"]').contains(data.text)
-        cy.get('[data-test="return-status-9"] > .govuk-tag').contains(data.label)
+        cy.get('[data-test="return-status-9"] > .govuk-tag').contains('void')
+
         cy.get('[data-test="return-due-date-10"]').contains(data.text)
         cy.get('[data-test="return-status-10"] > .govuk-tag').contains(data.label)
+
         cy.get('[data-test="return-due-date-11"]').contains(data.text)
-        cy.get('[data-test="return-status-11"] > .govuk-tag').contains('void')
+        cy.get('[data-test="return-status-11"] > .govuk-tag').contains(data.label)
+
         cy.get('[data-test="return-due-date-12"]').contains(data.text)
         cy.get('[data-test="return-status-12"] > .govuk-tag').contains(data.label)
+
         cy.get('[data-test="return-due-date-13"]').contains(data.text)
-        cy.get('[data-test="return-status-13"] > .govuk-tag').contains('void')
+        cy.get('[data-test="return-status-13"] > .govuk-tag').contains(data.label)
       })
     })
   })
