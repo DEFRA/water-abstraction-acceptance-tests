c4c3ba9d6968ca7b2610a2b5eaeb96f776ccd1a3
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-09-29 23:01:33 +0100

    Add users to the fixture builder data set
    We have some external tests that just need a user. There are also others which are using the barebones.json fixture, when something much simpler is needed.
    
    By adding a `users.js` to fixture builder we can move those across to the new way of doing things.

---
+++ cypress/support/fixture-builder/users.js
@@ -0,0 +1,20 @@
+export default function () {
+  return {
+    external: {
+      username: 'external@example.com',
+      password: 'P@55word',
+      resetRequired: 0,
+      application: 'water_vml',
+      badLogins: 0,
+      enabled: true
+    },
+    external1: {
+      username: 'external.shared@example.com',
+      password: 'P@55word',
+      resetRequired: 0,
+      application: 'water_vml',
+      badLogins: 0,
+      enabled: true
+    }
+  }
+}
