dfe74fbe73e2b4a973d87dcbc9ca1d9b9d63d04f
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-09-29 23:21:53 +0100

    Update the return statuses tests
    We've moved it over to the new data loading mechanism and programmatic login. But we've also created it its own scenario data.
    
    The previous test was simply confirming some of the statuses we expect were on the page somewhere. Now we load return logs with the intent of generating specific statuses on the page that we can then check for.

--- cypress/e2e/external/returns/return-statuses.cy.js
+++ cypress/e2e/external/returns/return-statuses.cy.js
@@ -1,46 +1,33 @@
 'use strict'
 
-describe('View returns (external)', () => {
+import externalReturnStatuses from '../../../support/scenarios/external-return-statuses.js'
+
+const externalReturnStatusesScenario = externalReturnStatuses()
+
+describe('Return statuses (external)', () => {
   beforeEach(() => {
     cy.tearDown()
-    cy.fixture('barebones.json').then((fixture) => {
-      const futureDate = new Date()
-      const additionalMonths = 3
-
-      futureDate.setMonth(futureDate.getMonth() + additionalMonths)
-
-      fixture.returnLogs[2].dueDate = futureDate.toISOString()
-      cy.load(fixture)
-    })
-
-    cy.fixture('external-user.json').then((fixture) => {
-      cy.load(fixture)
-    })
-    cy.fixture('users.json').its('loadedExternal').as('userEmail')
+    cy.load(externalReturnStatusesScenario)
+    cy.wrap(externalReturnStatusesScenario.users[0].username).as('userEmail')
   })
 
   it('login as an existing user and view returns', () => {
-    cy.visit(Cypress.env('externalUrl'))
-
-    // tap the sign in button on the welcome page
-    cy.get('a[href*="/signin"]').click()
-
-    //  Enter the user name and Password
     cy.get('@userEmail').then((userEmail) => {
-      cy.get('input#email').type(userEmail)
+      cy.programmaticLogin({
+        email: userEmail,
+        external: true
+      })
     })
-    cy.get('input#password').type(Cypress.env('defaultPassword'))
-
-    //  Click Sign in Button
-    cy.get('.govuk-button.govuk-button--start').click()
+    cy.visit(`${Cypress.env('externalUrl')}/licences`)
 
     // Select a licence to view returns and their status
-    cy.contains('AT/CURR/MONTHLY/02').click()
+    cy.contains('AT/CURR/DAILY/01').click()
     cy.get('#tab_returns').click()
     cy.get('#returns').should('be.visible')
-    cy.get('.govuk-tag').should('be.visible').and('contain.text', 'open')
-    cy.get('.govuk-tag').should('be.visible').and('contain.text', 'overdue')
-    cy.get('.govuk-tag').should('be.visible').and('contain.text', 'complete')
-    cy.get('.govuk-tag').should('be.visible').and('contain.text', 'overdue')
+    cy.get('.govuk-tag').should('not.contain.text', 'not yet due')
+    cy.get(':nth-child(1) > :nth-child(4) > .govuk-tag').should('be.visible').and('contain.text', 'overdue')
+    cy.get(':nth-child(2) > :nth-child(4) > .govuk-tag').should('be.visible').and('contain.text', 'due')
+    cy.get(':nth-child(3) > :nth-child(4) > .govuk-tag').should('be.visible').and('contain.text', 'open')
+    cy.get(':nth-child(4) > :nth-child(4) > .govuk-tag').should('be.visible').and('contain.text', 'complete')
   })
 })
---
+++ cypress/support/scenarios/external-return-statuses.js
@@ -0,0 +1,146 @@
+import licenceData from '../fixture-builder/licence.js'
+import usersData from '../fixture-builder/users.js'
+
+function _formatDateObjectToISO(date) {
+  return date.toISOString().split('T')[0]
+}
+
+function _returnLog(startDate, endDate, dueDate, returnReference) {
+  const startDateString = _formatDateObjectToISO(startDate)
+  const endDateString = _formatDateObjectToISO(endDate)
+  const dueDateString = dueDate ? _formatDateObjectToISO(dueDate) : null
+
+  return {
+    id: `v1:1:AT/CURR/DAILY/01:${returnReference}:${startDateString}:${endDateString}`,
+    returnReference: returnReference.toString(),
+    licenceRef: 'AT/CURR/DAILY/01',
+    metadata: {
+      nald: {
+        areaCode: 'AREA',
+        formatId: returnReference,
+        regionCode: 1,
+        periodEndDay: '31',
+        periodEndMonth: '12',
+        periodStartDay: '1',
+        periodStartMonth: '1'
+      },
+      points: [
+        {
+          name: 'The Name of this',
+          ngr1: 'TG 123 456',
+          ngr2: null,
+          ngr3: null,
+          ngr4: null
+        }
+      ],
+      isFinal: false,
+      version: 1,
+      isSummer: false,
+      isUpload: false,
+      purposes: [
+        {
+          alias: 'SPRAY IRRIGATION STORAGE',
+          primary: {
+            code: 'A',
+            description: 'Agriculture'
+          },
+          secondary: {
+            code: 'AGR',
+            description: 'General Agriculture'
+          },
+          tertiary: {
+            code: '420',
+            description: 'Spray Irrigation - Storage'
+          }
+        }
+      ],
+      isCurrent: true,
+      description: 'Its all about the description',
+      isTwoPartTariff: true
+    },
+    returnsFrequency: 'month',
+    startDate: startDateString,
+    endDate: endDateString,
+    dueDate: dueDateString,
+    status: 'due',
+    underQuery: false,
+    returnCycleId: {
+      schema: 'returns',
+      table: 'returnCycles',
+      lookup: 'startDate',
+      value: '2020-04-01',
+      select: 'returnCycleId'
+    }
+  }
+}
+
+function _returnLogs() {
+  const returnLogs = []
+  const today = new Date()
+
+  today.setHours(0, 0, 0, 0)
+
+  // Does the current date fall in April to December, or January to March.
+  const beforeNewYear = (today.getMonth() + 1) > 3
+
+  let startDate
+  if (beforeNewYear) {
+    startDate = new Date(`${today.getFullYear() + 1}-04-01`)
+  } else {
+    startDate = new Date(`${today.getFullYear()}-04-01`)
+  }
+
+  let endDate = new Date(`${startDate.getFullYear() + 1}-03-31`)
+  let dueDate = null
+
+  // NOT DUE YET - We've set the start and end date in the future so it cannot be submitted hence the status
+  let returnLog = _returnLog(startDate, endDate, dueDate, 9999990)
+  returnLogs.push(returnLog)
+
+  // COMPLETE - Set the period to last year for
+  startDate.setFullYear(startDate.getFullYear() - 2)
+  endDate.setFullYear(endDate.getFullYear() - 2)
+  dueDate = new Date(`${startDate.getFullYear()}-04-28`)
+
+  returnLog = _returnLog(startDate, endDate, dueDate, 9999991)
+  returnLog.status = 'completed'
+  returnLogs.push(returnLog)
+
+  // OPEN - Leave the date as is, which means it will be past its end date with a null due date
+  dueDate = null
+  returnLog = _returnLog(startDate, endDate, dueDate, 9999992)
+  returnLogs.push(returnLog)
+
+  // DUE - If the current month is April, we're going to leave the return log in last year. Else we'll bring it into
+  // the current year
+  if ((today.getMonth() + 1) !== 4) {
+    startDate.setFullYear(startDate.getFullYear() + 1)
+  }
+  // set the due date to be a few days in the future
+  dueDate = new Date(today)
+  dueDate.setDate(dueDate.getDate() + 5)
+
+  // set the end date to be a month ago
+  endDate = new Date(today)
+  endDate.setDate(today.getMonth() - 1)
+
+  returnLog = _returnLog(startDate, endDate, dueDate, 9999993)
+  returnLogs.push(returnLog)
+
+  // OVERDUE
+  dueDate.setDate(dueDate.getDate() - 6)
+  returnLog = _returnLog(startDate, endDate, dueDate, 9999994)
+  returnLogs.push(returnLog)
+
+  return returnLogs
+}
+
+export default function() {
+  return {
+    users: [
+      usersData().external
+    ],
+    ...licenceData(),
+    returnLogs: _returnLogs()
+  }
+}
