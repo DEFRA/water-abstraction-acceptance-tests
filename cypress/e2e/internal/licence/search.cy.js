000c84b38136d09455ab72b618c313c654f12383
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-09-30 18:38:46 +0100

    Update licence search
    We drop barebones and reuse one of our new scenarios. We also use programmatic login.

--- cypress/e2e/internal/licence/search.cy.js
+++ cypress/e2e/internal/licence/search.cy.js
@@ -1,84 +1,81 @@
 'use strict'
 
+import scenarioData from '../../../support/scenarios/one-licence-only.js'
+
+const scenario = scenarioData()
+
 describe('Search for a licence (internal)', () => {
   beforeEach(() => {
     cy.tearDown()
-    cy.fixture('barebones.json').then((fixture) => {
-      cy.load(fixture)
-    })
+
+    cy.load(scenario)
+
     cy.fixture('users.json').its('super').as('userEmail')
   })
 
   it('can find a licence using various search values', () => {
+    cy.get('@userEmail').then((userEmail) => {
+      cy.programmaticLogin({
+        email: userEmail
+      })
+    })
     cy.visit('/')
 
-    //  Enter the user name and Password
-    cy.get('@userEmail').then((userEmail) => {
-      cy.get('input#email').type(userEmail)
-    })
-    cy.get('input#password').type(Cypress.env('defaultPassword'))
-
-    //  Click Sign in Button
-    cy.get('.govuk-button.govuk-button--start').click()
-
-    //  Assert the user signed in and we're on the search page
-    cy.contains('Search')
-
     cy.log('Search for a license by using Licence holder Name')
-    cy.get('#query').type('barber bakery')
+    cy.get('#query').type('cupcake factory')
     cy.get('.search__button').click()
     cy.contains('Licences')
-    cy.get('.govuk-table__row').contains('AT/CURR/WEEKLY/01')
+    cy.get('.govuk-table__row').contains('AT/CURR/DAILY/01')
 
     cy.log('Search for a license by using Licence name or Licence alias')
     cy.get('#query').clear()
-    cy.get('#query').type(' weekly crumpet')
+    cy.get('#query').type('daily cupcake')
     cy.get('.search__button').click()
     cy.contains('Licences')
-    cy.get('.govuk-table__row').contains('AT/CURR/WEEKLY/01')
+    cy.get('.govuk-table__row').contains('AT/CURR/DAILY/01')
 
     cy.log('Search for a license by using Licence name or Licence alias in upper case')
     cy.get('#query').clear()
-    cy.get('#query').type(' WEEKLY CRUMPET')
+    cy.get('#query').type('DAILY CUPCAKE')
     cy.get('.search__button').click()
     cy.contains('Licences')
-    cy.get('.govuk-table__row').contains('AT/CURR/WEEKLY/01')
+    cy.get('.govuk-table__row').contains('AT/CURR/DAILY/01')
 
     cy.log('Search for a license by using Licence Number')
     cy.get('#query').clear()
-    cy.get('#query').type('AT/CURR/WEEKLY/01')
+    cy.get('#query').type('AT/CURR/DAILY/01')
     cy.get('.search__button').click()
     cy.contains('Licences')
-    cy.get('.govuk-table__row').contains('AT/CURR/WEEKLY/01')
+    cy.get('.govuk-table__row').contains('AT/CURR/DAILY/01')
 
     cy.log('Search for a license by using Licence Number case sensitive')
     cy.get('#query').clear()
-    cy.get('#query').type('at/CURR/WEEKLY/01')
+    cy.get('#query').type('at/CURR/DAILY/01')
     cy.get('.search__button').click()
     cy.contains('Licences')
-    cy.get('.govuk-table__row').contains('AT/CURR/WEEKLY/01')
+    cy.get('.govuk-table__row').contains('AT/CURR/DAILY/01')
 
     cy.log('Search for a license by using partial Licence Number')
     cy.get('#query').clear()
-    cy.get('#query').type('at/CURR/WEEKLY/0')
+    cy.get('#query').type('at/CURR/DAILY/0')
     cy.get('.search__button').click()
     cy.contains('Licences')
-    cy.get('.govuk-table__row').contains('AT/CURR/WEEKLY/01')
+    cy.get('.govuk-table__row').contains('AT/CURR/DAILY/01')
 
     cy.log('Search for a license by using partial name')
     cy.get('#query').clear()
-    cy.get('#query').type('shop')
+    cy.get('#query').type('factory')
     cy.get('.search__button').click()
     cy.contains('Licences')
-    cy.get('.govuk-table__body').contains('AT/CURR/MONTHLY/01')
-    cy.get('.govuk-table__body').contains('the monthly pie licence')
+    cy.get('.govuk-table__body').contains('AT/CURR/DAILY/01')
+    cy.get('.govuk-table__body').contains('the daily cupcake licence')
 
     cy.log('Search for a license by using partial name')
     cy.get('#query').clear()
-    cy.get('#query').type('doughnut store')
+    cy.get('#query').type('daily cupcake')
     cy.get('.search__button').click()
     cy.contains('Licences')
-    cy.get('.govuk-table__body').contains('AT/CURR/MONTHLY/02')
-    cy.get('.govuk-table__body').contains('the monthly doughnut licence')
+    cy.get('.govuk-table__body').contains('AT/CURR/DAILY/01')
+    cy.get('.govuk-table__body').contains('the daily cupcake licence')
   })
 })
