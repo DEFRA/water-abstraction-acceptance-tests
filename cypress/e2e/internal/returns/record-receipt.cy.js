afe469e0aa788b6869166e267a83011296aef490
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-10-01 16:50:00 +0100

    Update record receipt and submit return tests
    Drop use of barebones, use the new loader and programmatic login.

--- cypress/e2e/internal/returns/record-receipt.cy.js
+++ cypress/e2e/internal/returns/record-receipt.cy.js
@@ -1,48 +1,40 @@
 'use strict'
 
+import scenarioData from '../../../support/scenarios/internal-return-submission.js'
+
+const scenario = scenarioData()
+
 describe('Record receipt for return (internal)', () => {
   beforeEach(() => {
     cy.tearDown()
-    cy.fixture('barebones.json').then((fixture) => {
-      cy.load(fixture)
-    })
+
+    cy.load(scenario)
+
     cy.fixture('users.json').its('billingAndData').as('userEmail')
   })
 
   it('record the receipt for an overdue return for a licence from its returns tab', () => {
+    cy.get('@userEmail').then((userEmail) => {
+      cy.programmaticLogin({
+        email: userEmail
+      })
+    })
     cy.visit('/')
 
-    // enter the user name and Password
-    cy.get('@userEmail').then((userEmail) => {
-      cy.get('input#email').type(userEmail)
-    })
-    cy.get('input#password').type(Cypress.env('defaultPassword'))
-
-    // click Sign in Button
-    cy.get('.govuk-button.govuk-button--start').click()
-
-    // assert the user signed in and we're on the search page
-    cy.contains('Search')
-
-    // search for a licence
-    cy.get('#query').type('AT/CURR/MONTHLY/02')
+    // Search for the licence and select it from the results
+    cy.get('#query').type('AT/CURR/DAILY/01')
     cy.get('.search__button').click()
-    cy.get('.govuk-table__row').contains('AT/CURR/MONTHLY/02').click()
+    cy.get('.govuk-table__row').contains('AT/CURR/DAILY/01').click()
 
     // confirm we are on the licence page and select returns tab
-    cy.contains('AT/CURR/MONTHLY/02')
+    cy.contains('AT/CURR/DAILY/01')
     cy.get('[data-test="#tab_returns"]').click()
 
     // confirm we are on the tab page
     cy.get('#returns > .govuk-heading-l').contains('Returns')
 
-    // confirm we see the overdue return
-    cy.get('#returns').within(() => {
-      cy.get('.govuk-table__row:nth-child(5)').should('be.visible').and('contain.text', '9999990')
-      cy.get('.govuk-table__row:nth-child(5)').should('be.visible').and('contain.text', 'overdue')
-
-      cy.get('.govuk-table__row:nth-child(5) a').contains('9999990').click()
-    })
+    // Select the return log to record a receipt
+    cy.get('[data-test="return-reference-0"] > .govuk-link').click()
 
     // Abstraction return
     // submit return
@@ -60,20 +52,14 @@ describe('Record receipt for return (internal)', () => {
 
     // Return received
     cy.get('.govuk-panel').contains('Return 9999990 received').should('be.visible')
-    cy.get('.govuk-panel').contains('AT/CURR/MONTHLY/02').should('be.visible')
+    cy.get('.govuk-panel').contains('AT/CURR/DAILY/01').should('be.visible')
     cy.get('.govuk-panel').contains('Its all about the description').should('be.visible')
     cy.get('.govuk-panel').contains('Spray Irrigation - Storage').should('be.visible')
 
     // View returns for the licence (this is a different view)
-    cy.get('div > a').contains('View returns for AT/CURR/MONTHLY/02').should('be.visible').click()
+    cy.get('div > a').contains('View returns for AT/CURR/DAILY/01').should('be.visible').click()
 
     // confirm we see the received return
-    cy.get('.govuk-table > .govuk-table__body').within(() => {
-      cy.get('.govuk-table__row:nth-child(5)')
-        .should('be.visible')
-        .and('contain.text', '1 January 2019 to 31 December 2019')
-
-      cy.get('.govuk-table__row:nth-child(5)').should('be.visible').and('contain.text', 'received')
-    })
+    cy.get('[data-test="return-status-0"] > .govuk-tag').should('be.visible').and('contain.text', 'received')
   })
 })
--- cypress/e2e/internal/returns/submit.cy.js
+++ cypress/e2e/internal/returns/submit.cy.js
@@ -1,48 +1,40 @@
 'use strict'
 
+import scenarioData from '../../../support/scenarios/internal-return-submission.js'
+
+const scenario = scenarioData()
+
 describe('Submit a return (internal)', () => {
   beforeEach(() => {
     cy.tearDown()
-    cy.fixture('barebones.json').then((fixture) => {
-      cy.load(fixture)
-    })
+
+    cy.load(scenario)
+
     cy.fixture('users.json').its('billingAndData').as('userEmail')
   })
 
   it('submit a return for a licence from its returns tab and mark the licence for supplementary billing', () => {
+    cy.get('@userEmail').then((userEmail) => {
+      cy.programmaticLogin({
+        email: userEmail
+      })
+    })
     cy.visit('/')
 
-    // enter the user name and Password
-    cy.get('@userEmail').then((userEmail) => {
-      cy.get('input#email').type(userEmail)
-    })
-    cy.get('input#password').type(Cypress.env('defaultPassword'))
-
-    // click Sign in Button
-    cy.get('.govuk-button.govuk-button--start').click()
-
-    // assert the user signed in and we're on the search page
-    cy.contains('Search')
-
-    // search for a licence
-    cy.get('#query').type('AT/CURR/MONTHLY/02')
+    // Search for the licence and select it from the results
+    cy.get('#query').type('AT/CURR/DAILY/01')
     cy.get('.search__button').click()
-    cy.get('.govuk-table__row').contains('AT/CURR/MONTHLY/02').click()
+    cy.get('.govuk-table__row').contains('AT/CURR/DAILY/01').click()
 
     // confirm we are on the licence page and select returns tab
-    cy.contains('AT/CURR/MONTHLY/02')
+    cy.contains('AT/CURR/DAILY/01')
     cy.get('[data-test="#tab_returns"]').click()
 
     // confirm we are on the tab page
     cy.get('#returns > .govuk-heading-l').contains('Returns')
 
-    // confirm we see the due return
-    cy.get('#returns').within(() => {
-      cy.get('.govuk-table__row:nth-child(2)').should('be.visible').and('contain.text', '9999992')
-      cy.get('.govuk-table__row:nth-child(2)').should('be.visible').and('contain.text', 'overdue')
-
-      cy.get('.govuk-table__row:nth-child(2) a').contains('9999992').click()
-    })
+    // Select the return log to submit
+    cy.get('[data-test="return-reference-0"] > .govuk-link').click()
 
     // Abstraction return
     // submit return
@@ -91,7 +83,7 @@ describe('Submit a return (internal)', () => {
 
     // Return submitted
     // confirm we see the success panel and then click the Mark for supplementary bill run button
-    cy.get('.govuk-panel').should('contain.text', 'Return 9999992 submitted')
+    cy.get('.govuk-panel').should('contain.text', 'Return 9999990 submitted')
     cy.get('.govuk-button').contains('Mark for supplementary bill run').click()
 
     // Summary
