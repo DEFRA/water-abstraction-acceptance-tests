61d5fdee45c6b89b763d7cfe6480ad58a0326387
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-10-01 16:55:53 +0100

    Update view-status test

--- cypress/e2e/internal/returns/view-status.cy.js
+++ cypress/e2e/internal/returns/view-status.cy.js
@@ -1,57 +1,55 @@
 'use strict'
 
+import scenarioData from '../../../support/scenarios/return-statuses.js'
+
+const scenario = scenarioData()
+
 describe('View returns and their status (internal)', () => {
   beforeEach(() => {
     cy.tearDown()
-    cy.fixture('barebones.json').then((fixture) => {
-      cy.load(fixture)
-    })
+
+    cy.load(scenario)
+
     cy.fixture('users.json').its('billingAndData').as('userEmail')
   })
 
   it('lists the returns for a licence and their status', () => {
+    cy.get('@userEmail').then((userEmail) => {
+      cy.programmaticLogin({
+        email: userEmail
+      })
+    })
     cy.visit('/')
 
-    // enter the user name and Password
-    cy.get('@userEmail').then((userEmail) => {
-      cy.get('input#email').type(userEmail)
-    })
-    cy.get('input#password').type(Cypress.env('defaultPassword'))
-
-    // click Sign in Button
-    cy.get('.govuk-button.govuk-button--start').click()
-
-    // assert the user signed in and we're on the search page
-    cy.contains('Search')
-
-    // search for a licence
-    cy.get('#query').type('AT/CURR/MONTHLY/02')
+    // Search for the licence and select it from the results
+    cy.get('#query').type('AT/CURR/DAILY/01')
     cy.get('.search__button').click()
-    cy.get('.govuk-table__row').contains('AT/CURR/MONTHLY/02').click()
+    cy.get('.govuk-table__row').contains('AT/CURR/DAILY/01').click()
 
     // confirm we are on the licence page and select returns tab
-    cy.contains('AT/CURR/MONTHLY/02')
+    cy.contains('AT/CURR/DAILY/01')
     cy.get('[data-test="#tab_returns"]').click()
 
     // confirm we are on the tab page
     cy.get('#returns > .govuk-heading-l').contains('Returns')
 
     // confirm we see the expected returns and their statuses
-    cy.get('#returns').within(() => {
-      cy.get('.govuk-table__row:nth-child(1)').should('be.visible').and('contain.text', '9999993')
-      cy.get('.govuk-table__row:nth-child(1)').should('be.visible').and('contain.text', 'void')
+    cy.get('[data-test="return-reference-0"]').should('be.visible').and('contain.text', '9999990')
+    cy.get('[data-test="return-status-0"] > .govuk-tag').should('be.visible').and('contain.text', 'not due yet')
 
-      cy.get('.govuk-table__row:nth-child(2)').should('be.visible').and('contain.text', '9999992')
-      cy.get('.govuk-table__row:nth-child(2)').should('be.visible').and('contain.text', 'overdue')
+    cy.get('[data-test="return-reference-1"]').should('be.visible').and('contain.text', '9999995')
+    cy.get('[data-test="return-status-1"] > .govuk-tag').should('be.visible').and('contain.text', 'overdue')
 
-      cy.get('.govuk-table__row:nth-child(3)').should('be.visible').and('contain.text', '9999992')
-      cy.get('.govuk-table__row:nth-child(3)').should('be.visible').and('contain.text', 'complete')
+    cy.get('[data-test="return-reference-2"]').should('be.visible').and('contain.text', '9999994')
+    cy.get('[data-test="return-status-2"] > .govuk-tag').should('be.visible').and('contain.text', 'due')
 
-      cy.get('.govuk-table__row:nth-child(4)').should('be.visible').and('contain.text', '9999991')
-      cy.get('.govuk-table__row:nth-child(4)').should('be.visible').and('contain.text', 'overdue')
+    cy.get('[data-test="return-reference-3"]').should('be.visible').and('contain.text', '9999993')
+    cy.get('[data-test="return-status-3"] > .govuk-tag').should('be.visible').and('contain.text', 'open')
 
-      cy.get('.govuk-table__row:nth-child(5)').should('be.visible').and('contain.text', '9999990')
-      cy.get('.govuk-table__row:nth-child(5)').should('be.visible').and('contain.text', 'overdue')
-    })
+    cy.get('[data-test="return-reference-4"]').should('be.visible').and('contain.text', '9999992')
+    cy.get('[data-test="return-status-4"] > .govuk-tag').should('be.visible').and('contain.text', 'void')
+
+    cy.get('[data-test="return-reference-5"]').should('be.visible').and('contain.text', '9999991')
+    cy.get('[data-test="return-status-5"] > .govuk-tag').should('be.visible').and('contain.text', 'complete')
   })
 })
