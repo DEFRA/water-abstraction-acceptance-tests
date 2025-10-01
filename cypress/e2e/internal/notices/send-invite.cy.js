b34d3d3c2530e6f0c1cbf9fb44fc6b28371c2cea
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-10-01 15:40:17 +0100

    Update send returns invite and reminder tests
    Now we seed data that will ensure we can always run these tests, rather than rely on a team member having the necessary return logs in place.

--- cypress/e2e/internal/notices/send-invite.cy.js
+++ cypress/e2e/internal/notices/send-invite.cy.js
@@ -1,31 +1,30 @@
 'use strict'
 
+import scenarioData from '../../../support/scenarios/return-notices.js'
+
 describe('Send returns invite to customer (internal)', () => {
   beforeEach(() => {
     cy.tearDown()
-    cy.fixture('barebones.json').then((fixture) => {
-      cy.load(fixture)
+    cy.calculatedDates().then((body) => {
+      cy.log(body)
+
+      const scenario = scenarioData(body.firstReturnPeriod)
+
+      cy.load(scenario)
     })
+
     cy.fixture('users.json').its('billingAndData').as('userEmail')
   })
 
   it('invites a customer to submit returns', () => {
-    cy.visit('/')
-
-    // enter the user name and Password
     cy.get('@userEmail').then((userEmail) => {
-      cy.get('input#email').type(userEmail)
+      cy.programmaticLogin({
+        email: userEmail
+      })
     })
-    cy.get('input#password').type(Cypress.env('defaultPassword'))
 
-    // click Sign in Button
-    cy.get('.govuk-button.govuk-button--start').click()
-
-    // assert the user signed in and we're on the search page
-    cy.contains('Search')
-
-    // navigate to the invitations flow
-    cy.get('#navbar-notifications').click()
+    // Navigate to the returns invitations flow.
+    cy.visit('/system/manage')
     cy.get('a[href="/system/notices/setup/standard?noticeType=invitations"]').click()
 
     // Select the returns periods for the standard
---
+++ cypress/support/scenarios/return-notices.js
@@ -0,0 +1,76 @@
+import formatDateToIso from '../helpers/formatDateToIso.js'
+import licenceData from '../fixture-builder/licence.js'
+
+export default function(returnPeriod) {
+  const startDateString = formatDateToIso(returnPeriod.startDate)
+  const endDateString = formatDateToIso(returnPeriod.endDate)
+  const dueDateString = formatDateToIso(returnPeriod.dueDate)
+
+  return {
+    ...licenceData(),
+    returnLogs: [
+      {
+        id: `v1:1:AT/CURR/DAILY/01:9999990:${startDateString}:${endDateString}`,
+        returnReference: '9999990',
+        licenceRef: 'AT/CURR/DAILY/01',
+        metadata: {
+          nald: {
+            areaCode: 'AREA',
+            formatId: 9999990,
+            regionCode: 9,
+            periodEndDay: '31',
+            periodEndMonth: '12',
+            periodStartDay: '1',
+            periodStartMonth: '1'
+          },
+          points: [
+            {
+              name: 'The Name of this',
+              ngr1: 'TG 123 456',
+              ngr2: null,
+              ngr3: null,
+              ngr4: null
+            }
+          ],
+          isFinal: false,
+          version: 1,
+          isSummer: false,
+          isUpload: false,
+          purposes: [
+            {
+              alias: 'SPRAY IRRIGATION STORAGE',
+              primary: {
+                code: 'A',
+                description: 'Agriculture'
+              },
+              secondary: {
+                code: 'AGR',
+                description: 'General Agriculture'
+              },
+              tertiary: {
+                code: '420',
+                description: 'Spray Irrigation - Storage'
+              }
+            }
+          ],
+          isCurrent: true,
+          description: 'Its all about the description',
+          isTwoPartTariff: true
+        },
+        returnsFrequency: 'month',
+        startDate: startDateString,
+        endDate: endDateString,
+        dueDate: dueDateString,
+        status: 'due',
+        underQuery: false,
+        returnCycleId: {
+          schema: 'returns',
+          table: 'returnCycles',
+          lookup: 'startDate',
+          value: '2020-04-01',
+          select: 'returnCycleId'
+        }
+      }
+    ]
+  }
+}
