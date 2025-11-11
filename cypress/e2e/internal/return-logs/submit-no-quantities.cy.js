3a5a0634976a225e408df0d584ae168125f15d03
Jason Claxton <jason.claxton@defra.gov.uk>
2025-11-11 09:59:54 +0000

    Create test for no quantities recorded

---
+++ cypress/e2e/internal/return-logs/submit-no-quantities.cy.js
@@ -0,0 +1,66 @@
+'use strict'
+
+import scenarioData from '../../../support/scenarios/internal-return-submission.js'
+
+const scenario = scenarioData()
+
+describe('Submit a return with no quantities - validation errors (internal)', () => {
+  beforeEach(() => {
+    cy.tearDown()
+
+    cy.load(scenario)
+
+    cy.fixture('users.json').its('billingAndData').as('userEmail')
+  })
+
+  it('submit a return for a licence from its returns tab and mark the licence for supplementary billing', () => {
+    cy.get('@userEmail').then((userEmail) => {
+      cy.programmaticLogin({
+        email: userEmail
+      })
+    })
+    cy.visit(`/system/return-logs/${scenario.returnLogs[0].returnId}`)
+
+    // Abstraction return
+    // submit return
+    cy.get('form > .govuk-button').first().click()
+
+    // When was the return received?
+    // select today
+    cy.get('#today').click()
+    cy.get('form > .govuk-button').click()
+
+    // What do you want to do with this return?
+    // choose Enter and submit and continue
+    cy.get('#enterReturn').click()
+    cy.get('form > .govuk-button').click()
+
+    // How was this return reported?
+    // choose Abstraction volumes and continue
+    cy.get('#abstractionVolumes').click()
+    cy.get('form > .govuk-button').click()
+
+    // Which units were used?
+    // choose Cubic metres and continue
+    cy.get('#cubicMetres').check()
+    cy.get('form > .govuk-button').click()
+
+    // Have meter details been provided?
+    // choose No and continue
+    cy.get('#no').click()
+    cy.get('form > .govuk-button').click()
+
+    // Is it a single volume?
+    // choose No
+    cy.get('#no').click()
+    cy.get('form > .govuk-button').click()
+
+    // Check details and enter new volumes or readings
+    // attempt to confirm the return without entering any quantities
+    cy.get('button.govuk-button').click()
+
+    // Validation error
+    // it should not be possible to submit a return without entering at least one quantity, even if it is a zero
+    cy.get('.govuk-error-summary').contains('At least one return line must contain a value.')
+  })
+})
