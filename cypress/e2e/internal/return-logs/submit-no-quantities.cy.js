c8aa24ab2207efc4ed1f0b3c58deb0390a38ff51
Jason Claxton <jason.claxton@defra.gov.uk>
2025-11-11 13:58:56 +0000

    Tidy up text

--- cypress/e2e/internal/return-logs/submit-no-quantities.cy.js
+++ cypress/e2e/internal/return-logs/submit-no-quantities.cy.js
@@ -13,7 +13,7 @@ describe('Submit a return with no quantities - validation errors (internal)', ()
     cy.fixture('users.json').its('billingAndData').as('userEmail')
   })
 
-  it('submit a return for a licence from its returns tab and mark the licence for supplementary billing', () => {
+  it('attempt to submit a return for a licence from its returns tab without entering any quantities', () => {
     cy.get('@userEmail').then((userEmail) => {
       cy.programmaticLogin({
         email: userEmail
