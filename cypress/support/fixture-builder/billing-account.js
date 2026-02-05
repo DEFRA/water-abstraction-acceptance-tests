fc99fae7e49cdd829e08e1e03ebfe2f5d1f2193d
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-02-05 16:42:43 +0000

    Add BillingAccount fixture builder
    In order to create and send a bill run there is a bunch of records we need.
    
    The current bill run tests rely on fixed feature files bu the team are trying to move to the fixture builder and scenario files.
    
    So for this new test we'll aim to use them for everything we need, starting with the billing account.

---
+++ cypress/support/fixture-builder/billing-account.js
@@ -0,0 +1,7 @@
+export default function billingAccount () {
+  return {
+    id: '16cb50a5-e3e6-41f4-a42b-9dad6a69fc0c',
+    accountNumber: 'A99999991A',
+    companyId: 'e8abdbb4-aeea-47d4-91b2-97bf82bc2778'
+  }
+}
