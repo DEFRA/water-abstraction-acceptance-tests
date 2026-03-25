114d089f205e4882f993db4ad6ca6acdb6459d8c
Jason Claxton <jason.claxton@defra.gov.uk>
2026-03-25 16:47:11 +0000

    Update "Reset password journey (external)"

--- cypress/e2e/external/reset-password.cy.js
+++ cypress/e2e/external/reset-password.cy.js
@@ -39,6 +39,12 @@ describe('Reset password journey (external)', () => {
         cy.get('[id=confirmPassword]').type(newPassword)
         cy.get('button.govuk-button').click()
 
+        // Log back in using the updated credentials to confirm the password has been updated
+        cy.get('#email').type(userEmail)
+        cy.get('#password').type(newPassword)
+        cy.get('button.govuk-button').click()
+
+        // Assert that the user is logged in and on the dashboard page
         cy.contains('View licences').should('have.attr', 'href', '/licences')
         cy.contains('Add your licences to the service').should('have.class', 'govuk-heading-l')
       })
