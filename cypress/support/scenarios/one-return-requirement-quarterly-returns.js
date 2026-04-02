bc64f89f461b5dcd96628233a3e129bf874c7ff1
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-04-02 13:57:41 +0100

    Update scenarios to use return requirement data
    The idea is that the return logs we're seeding were built from the return requirements we're also seeding.
    
    However, when seeding the return logs, we weren't always referring to our seeded return requirements. An obvious one being the return reference.
    
    The view licence returns page orders returns logs by start date and reference, however, the returns logs we were seeding all used the same return reference.
    
    Updating them to match the return requirement they are supposed to be linked to, means we can rely on the order being consistent.

--- cypress/support/scenarios/one-return-requirement-quarterly-returns.js
+++ cypress/support/scenarios/one-return-requirement-quarterly-returns.js
@@ -23,29 +23,45 @@ export default function basicLicenceOneReturnRequirementQuarterlyReturns () {
   const startYear = currentFinancialYearInfo.start.year
   const endYear = currentFinancialYearInfo.end.year
 
-  dataModel.returnLogs[0].returnId = `v1:9:AT/TE/ST/01/01:9999990:${startYear + 1}-01-01:${endYear}-03-31`
   dataModel.returnLogs[0].dueDate = `${endYear}-04-28`
   dataModel.returnLogs[0].endDate = `${endYear}-03-31`
-  dataModel.returnLogs[0].startDate = `${endYear}-01-01`
+  dataModel.returnLogs[0].metadata.description = dataModel.returnRequirements[0].siteDescription
+  dataModel.returnLogs[0].metadata.isSummer = dataModel.returnRequirements[0].summer
   dataModel.returnLogs[0].returnCycleId.value = `${startYear}-04-01`
+  dataModel.returnLogs[0].returnId = `v1:9:AT/TE/ST/01/01:9999990:${startYear + 1}-01-01:${endYear}-03-31`
+  dataModel.returnLogs[0].returnReference = dataModel.returnRequirements[0].legacyId
+  dataModel.returnLogs[0].returnRequirementId = dataModel.returnRequirements[0].id
+  dataModel.returnLogs[0].startDate = `${endYear}-01-01`
 
-  dataModel.returnLogs[1].returnId = `v1:9:AT/TE/ST/01/01:9999990:${startYear}-10-01:${startYear}-12-31`
   dataModel.returnLogs[1].dueDate = `${endYear}-01-28`
   dataModel.returnLogs[1].endDate = `${startYear}-12-31`
-  dataModel.returnLogs[1].startDate = `${startYear}-10-01`
+  dataModel.returnLogs[1].metadata.description = dataModel.returnRequirements[0].siteDescription
+  dataModel.returnLogs[1].metadata.isSummer = dataModel.returnRequirements[0].summer
   dataModel.returnLogs[1].returnCycleId.value = `${startYear}-04-01`
+  dataModel.returnLogs[1].returnId = `v1:9:AT/TE/ST/01/01:9999990:${startYear}-10-01:${startYear}-12-31`
+  dataModel.returnLogs[1].returnReference = dataModel.returnRequirements[0].legacyId
+  dataModel.returnLogs[1].returnRequirementId = dataModel.returnRequirements[0].id
+  dataModel.returnLogs[1].startDate = `${startYear}-10-01`
 
-  dataModel.returnLogs[2].returnId = `v1:9:AT/TE/ST/01/01:9999990:${startYear}-07-01:${startYear}-09-30`
   dataModel.returnLogs[2].dueDate = `${startYear}-10-28`
   dataModel.returnLogs[2].endDate = `${startYear}-09-30`
-  dataModel.returnLogs[2].startDate = `${startYear}-07-01`
+  dataModel.returnLogs[2].metadata.description = dataModel.returnRequirements[0].siteDescription
+  dataModel.returnLogs[2].metadata.isSummer = dataModel.returnRequirements[0].summer
   dataModel.returnLogs[2].returnCycleId.value = `${startYear}-04-01`
+  dataModel.returnLogs[2].returnId = `v1:9:AT/TE/ST/01/01:9999990:${startYear}-07-01:${startYear}-09-30`
+  dataModel.returnLogs[2].returnReference = dataModel.returnRequirements[0].legacyId
+  dataModel.returnLogs[2].returnRequirementId = dataModel.returnRequirements[0].id
+  dataModel.returnLogs[2].startDate = `${startYear}-07-01`
 
-  dataModel.returnLogs[3].returnId = `v1:9:AT/TE/ST/01/01:9999990:${startYear}-04-01:${startYear}-06-30`
   dataModel.returnLogs[3].dueDate = `${startYear}-07-28`
   dataModel.returnLogs[3].endDate = `${startYear}-06-30`
-  dataModel.returnLogs[3].startDate = `${startYear}-04-01`
+  dataModel.returnLogs[3].metadata.description = dataModel.returnRequirements[0].siteDescription
+  dataModel.returnLogs[3].metadata.isSummer = dataModel.returnRequirements[0].summer
   dataModel.returnLogs[3].returnCycleId.value = `${startYear}-04-01`
+  dataModel.returnLogs[3].returnId = `v1:9:AT/TE/ST/01/01:9999990:${startYear}-04-01:${startYear}-06-30`
+  dataModel.returnLogs[3].returnReference = dataModel.returnRequirements[0].legacyId
+  dataModel.returnLogs[3].returnRequirementId = dataModel.returnRequirements[0].id
+  dataModel.returnLogs[3].startDate = `${startYear}-04-01`
 
   return dataModel
 }
