0818d405e891e77a905fe33993e4b60f7eb1ef72
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-04-02 17:38:44 +0100

    Add a manual additional recipient to adhoc paper
    Our address lookup is actually covered by the ad-hoc returns invitation test, because it includes adding an additional letter recipient that uses the address lookup.
    
    The existing address lookup test covers manual addresses. So, to kill two birds with one stone, we update the ad-hoc paper returns test to add an additional letter recipient that uses the manual address entry.

--- cypress/e2e/internal/notices/ad-hoc/paper-returns.cy.js
+++ cypress/e2e/internal/notices/ad-hoc/paper-returns.cy.js
@@ -49,10 +49,79 @@ describe('Ad-hoc Paper returns journey (internal)', () => {
     cy.get('[data-test="returns-notice-type"]').should('contain.text', 'Paper return')
     cy.contains('Confirm').click()
 
+    // Capture the notice reference so we can verify it later
+    cy.contains('.govuk-caption-l', 'Notice').invoke('text').then((text) => {
+      cy.wrap(text.trim()).as('noticeReference')
+    })
+
+    // Recipients count
+    cy.contains('Showing all 1 recipients')
+
+    // Add an additional recipient
+    cy.contains('Manage recipients').click()
+    cy.contains('Set up a single use address').click()
+
+    // Enter the recipient's name
+    cy.get('#name').type('Manual Recipient')
+    cy.contains('Continue').click()
+
+    // Enter a UK postcode
+    cy.get('#postcode').type('BS1 5AH')
+    cy.contains('Find addresses').click()
+
+    // Select the address returned from the lookup (rate limited so pause briefly)
+    // we have to wait a second. Both the lookup and selecting the address result in a call to the address facade which
+    // has rate monitoring protection. Because we're automating the calls, they happen to quickly so the facade rejects
+    // the second call. Hence we need to wait a second.
+    cy.wait(1000)
+    cy.contains('I cannot find the address in the list').click()
+
+    // Enter the address
+    cy.get('#addressLine1').type('4 Privet drive')
+    cy.get('#addressLine2').type('Little Whinging')
+    cy.get('#addressLine3').type('Surrey')
+    cy.get('#postcode').clear().type('WD25 7LR')
+    cy.contains('Continue').click()
+
+    // Recipients count
+    cy.contains('Showing all 2 recipients')
+
+    // Additional recipient is shown in the list
+    cy.get('[data-test^="recipient-contact"]').should('have.length', 2)
+    cy.get('[data-test="recipient-contact-1"]').within(() => {
+      cy.contains('Manual Recipient')
+      cy.contains('4 Privet drive')
+      cy.contains('Little Whinging')
+      cy.contains('Surrey')
+      cy.contains('WD25 7LR')
+    })
+    cy.get('[data-test="recipient-licence-numbers-1"]').should('contain.text', 'AT/TE/ST/01/01')
+    cy.get('[data-test="recipient-method-1"]').should('contain.text', 'Letter - single use')
+    cy.get('[data-test="recipient-action-1"]').within(() => {
+      cy.contains('Preview')
+    })
+
     // Check the recipients
     cy.contains('Send').click()
 
     // Notice confirmation
     cy.get('.govuk-panel__title', { timeout: 15000 }).contains('Paper returns sent')
+    cy.get('.govuk-link').contains('View notice').click()
+
+    // Notice page contains the recipients
+    cy.get('@noticeReference').then((noticeReference) => {
+      cy.contains('.govuk-caption-l', noticeReference)
+    })
+
+    cy.contains('Showing all 2 notifications')
+
+    cy.get('[data-test^="notification-recipient"]').should('have.length', 2)
+    cy.get('[data-test="notification-recipient1"]').within(() => {
+      cy.contains('Manual Recipient')
+      cy.contains('4 Privet drive')
+      cy.contains('Little Whinging')
+      cy.contains('Surrey')
+      cy.contains('WD25 7LR')
+    })
   })
 })
