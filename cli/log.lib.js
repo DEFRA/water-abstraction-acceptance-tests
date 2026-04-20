e61b9ba1664ded4a445f95e4e0ab9ad044d71961
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2026-04-20 11:23:56 +0100

    Add new CLI tool to 'just' teardown and seed data (#225)
    https://eaflood.atlassian.net/browse/WATER-5602
    
    A typical app starts with a blank slate. That's great for acceptance testing, as though it is not 'good practice', you can create the data you need for the scenario you are testing via the UI.
    
    In WRLS, most of the data we rely on is imported from NALD. There is no way to create or modify this data via the UI. But we need it in order to test the service.
    
    This means our acceptance tests have always relied on a way to 'seed' data in order to test a scenario.
    
    The previous team had a way, and we replaced that with an improved one. But in both cases, the acceptance tests follow the same pattern
    
    - Teardown any existing test data
    - Seed data needed for the scenario being tested
    - Interact with the service via its UI, asserting its behaviour is as expected
    
    Some scenarios are very complex and often difficult to replicate with 'actual' data. When working on a feature or bug, you need data in a specific state. You can invest a lot of time trying to find, for example, a licence with the properties you need, only to have one shot at testing because you’ve now changed the data.
    
    **Seed-only testing**
    
    Some members of the team realised that the acceptance tests can seed 'just' the right data they need. But the thing they are testing or exploring is different to what the test is doing.
    
    So, they would comment out the 'test' part of the test, and run it just to access the teardown and seeding.
    
    This meant they could look into an issue quickly and repeat their testing with ease.
    
    **A new CLI tool**
    
    This changes adds a new CLI tool the project. You can access this from the command line via `npm run cli:seed` or via VSCode tasks.
    
    It will return a list of scenarios based on the files found in `cypress/support/scenarios`. You can begin typing to filter the list if you know which scenario you want to load.
    
    Once selected, the tool will run the teardown first, just as the tests do, then load the selected scenario.
    
    If you change your mind, simply hit `ESC` to cancel before making a selection.
    
    Right now, it relies on knowledge of which scenarios exist and the file names previously chosen (they could be better!)
    
    If this is really useful, we’ll look to improve both in future. For now, it solves the immediate problem.
    
    There is also nothing stopping folks from adding new scenarios. They do not have to be used in a test to be included. They can just be used for manual or exploratory testing and loaded via the `cli:seed`.
    
    We hope this encourages a move to building scenarios while working on new or updated features. This will make adding new acceptance tests much easier, as the work of creating suitable test data is done!

---
+++ cli/log.lib.js
@@ -0,0 +1,47 @@
+/**
+ * Colorize log output for better readability
+ *
+ * This is a simple wrapper around console.log that adds color and style.
+ *
+ * It is more common to add a dependency like {@link https://github.com/chalk/chalk|Chalk} in order to color and style
+ * your CLI. But we have a very simple CLI as a helper in an acceptance test project. We try to avoid adding
+ * dependencies where possible, especially in light of recent supply chain attacks. We can recreate what we would do
+ * with chalk via this little module, that uses ANSI escape codes.
+ *
+ * > See {@link https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797|ANSI escape codes} for more details on
+ * > how this works.
+ *
+ * @module LogLib
+ */
+
+const BOLD = '\x1b[1m'
+const BLUE = '\x1b[34m'
+const GREEN = '\x1b[32m'
+const RED = '\x1b[31m'
+const RESET_ALL = '\x1b[0m'
+const RESET_BOLD = '\x1b[22m'
+const YELLOW = '\x1b[33m'
+
+export function logInfo (message) {
+  _log(BLUE, message)
+}
+
+export function logError (message) {
+  _log(RED, message)
+}
+
+export function logSuccess (message) {
+  _log(GREEN, message)
+}
+
+export function logWarning (message) {
+  _log(YELLOW, message)
+}
+
+export function styleBold (message) {
+  return `${BOLD}${message}${RESET_BOLD}`
+}
+
+function _log (color, message) {
+  console.log(`${color}${message}${RESET_ALL}`)
+}
---
+++ cli/seed.cli.js
@@ -0,0 +1,148 @@
+/**
+ * Load test scenarios for use with manual exploratory testing
+ *
+ * This script provides an interactive CLI to seed the local database with specific test scenarios. It automates the
+ * "Tear Down" and "Data Load" cycles.
+ *
+ * To run this script use
+ *
+ * ```bash
+ * npm run cli:seed
+ * ```
+ *
+ * > Defined in `package.json` as `"cli:seed": "node cli/seed.cli.js"`
+ *
+ * @module SeedCLI
+ */
+
+import fs from 'fs'
+import path from 'path'
+import { search } from '@inquirer/prompts'
+
+import { logError, logInfo, logSuccess, logWarning, styleBold } from './log.lib.js'
+import { post } from './system.request.js'
+
+const ESCAPE_KEY_ABORT_CONTROLLER = new AbortController()
+const SCENARIOS_DIR = 'cypress/support/scenarios'
+
+async function run () {
+  logInfo(styleBold('Use this tool to load test scenarios for manual exploratory testing\n'))
+
+  const scenarios = _scenarios()
+
+  const selectedScenario = await _prompt(scenarios)
+
+  await _tearDown()
+
+  const body = await _body(selectedScenario)
+
+  await _load(selectedScenario, body)
+
+  logSuccess('Finished!')
+}
+
+async function _body (selectedScenario) {
+  // 1. Get the absolute path
+  const scenarioPath = path.resolve(SCENARIOS_DIR, `${selectedScenario}.js`)
+
+  // 2. Use dynamic import() to load the ESM scenario file.
+  // This allows the scenario to 'import' other ESM files (like licence.js)
+  const scenarioModule = await import(`file://${scenarioPath}`)
+
+  // 3. Handle the export (ESM uses .default)
+  const getBody = scenarioModule.default || scenarioModule
+
+  if (typeof getBody !== 'function') {
+    throw new Error(`The file "${selectedScenario}.js" must have an "export default" function.`)
+  }
+
+  // 4. Call the function here to get the actual data object
+  return await getBody()
+}
+
+async function _load (selectedScenario, body) {
+  logInfo(`Loading scenario ${styleBold(selectedScenario)}...`)
+
+  await post('/system/data/load', body)
+}
+
+/**
+ * Generate the CLI prompt the user will use to select the scenario to load
+ *
+ * {@link https://github.com/SBoudrias/Inquirer.js|@inquirer/prompts} is the package we use to create the CLI prompt. It
+ * has a lot of different prompt types, but we use the `search` type which gives us a nice searchable dropdown in the
+ * terminal.
+ *
+ * We have to provide 2 args to `search()`. The first is 'options' which defines how the prompt looks and behaves. It
+ * must contain a `message` which is the text shown to the user when asking the question, and a `source` function which
+ * is called each time the user types to get the list of options to show in the dropdown.
+ *
+ * The second is an object that can be used to pass in an `AbortSignal`, which we use to allow the user to cancel the
+ * prompt by pressing the Escape key.
+ *
+ * The function we provide to `source` filters the list of scenarios based on the user's input, and then maps them into
+ * the format expected by `search()`.
+ *
+ * Normally we would declare the function elsewhere to simplify things in _`prompt()`. However, we take advantage of a
+ * closure here so that `source` has access to the `scenarios` variable without us having to pass it in as an argument.
+ *
+ * If we didn't, we would either have to fetch all the possible scenarios on each key press, or declare it globally
+ * within the module.
+ *
+ * @private
+ */
+async function _prompt (scenarios) {
+  return search(
+    {
+      message: 'Type to search scenarios:',
+      source: async (input) => {
+        let filteredScenarios = scenarios
+
+        if (input) {
+          filteredScenarios = scenarios.filter((scenario) => {
+            return scenario.toLowerCase().includes(input.toLowerCase())
+          })
+        }
+
+        return filteredScenarios
+          .map((scenario) => {
+            return { name: scenario, value: scenario }
+          })
+      }
+    },
+    { signal: ESCAPE_KEY_ABORT_CONTROLLER.signal }
+  )
+}
+
+function _scenarios () {
+  return fs.readdirSync(SCENARIOS_DIR)
+    .filter((file) => {
+      return file.endsWith('.js')
+    })
+    .map((file) => {
+      return file.replace('.js', '')
+    })
+}
+
+async function _tearDown () {
+  logInfo('Tearing down previous scenario data...')
+
+  await post('/system/data/tear-down')
+}
+
+process.stdin.on('keypress', (str, key) => {
+  if (key.name === 'escape') {
+    ESCAPE_KEY_ABORT_CONTROLLER.abort()
+  }
+})
+
+try {
+  await run()
+} catch (err) {
+  if (['AbortPromptError', 'ExitPromptError'].includes(err.name)) {
+    logWarning('\nCancelled')
+  } else {
+    logError(err.message)
+    process.exit(1) // Standard practice to exit with failure code
+  }
+}
