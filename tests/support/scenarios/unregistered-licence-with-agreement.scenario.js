3bcf523c23fa2b6b6fb9d5aba94477dd9e42ac72
jonathangoulding <goulding.jon@icloud.com>
2026-07-06 15:28:47 +0100

    refactor: scenarios

--- tests/support/scenarios/unregistered-licence-with-agreement.scenario.js
+++ tests/support/scenarios/unregistered-licence-with-agreement.scenario.js
@@ -2,8 +2,9 @@ import licenceAgreementData from '../data/licence-agreement.data.js'
 import unregisteredLicenceScenario from './unregistered-licence.scenario.js'
 import { mergeByKey } from '../helpers/scenario.helpers.js'
 
-export const title = 'Licence with an agreement'
-export const description = 'A licence, licence holder, company and a section 127 two-part tariff agreement'
+export const title = 'Unregistered licence with an agreement'
+export const description =
+  'An unregistered licence, licence holder, company and a section 127 two-part tariff agreement'
 
 export default function () {
   const unregisteredLicence = unregisteredLicenceScenario()
