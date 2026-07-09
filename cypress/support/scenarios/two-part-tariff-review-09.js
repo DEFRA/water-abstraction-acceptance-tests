eccbe1ac18eb8e14368161a1e0d8cb20db6a9364
Jason Claxton <jason.claxton@defra.gov.uk>
2026-07-09 14:27:41 +0100

    Update scenario 9

---
+++ cypress/support/scenarios/two-part-tariff-review-09.js
@@ -0,0 +1,231 @@
+import reviewLicenceData from '../fixture-builder/review-licence.js'
+
+export const title = 'Two-part tariff review 09'
+export const description =
+  'Testing a two-part tariff bill run with a licence that is current and not in workflow, it has one applicable charge version with two charge references, each with one charge element. Both elements have a matching return that has a status of "due"'
+
+export default function () {
+  return {
+    ...reviewLicenceData(),
+    chargeVersions: [
+      {
+        id: '8e5626ee-5e4c-48f6-a668-471d35997e2c',
+        licenceId: 'f8702a6a-f61d-4b0a-9af3-9a53768ee516',
+        licenceRef: 'AT/TES/01',
+        billingAccountId: '16cb50a5-e3e6-41f4-a42b-9dad6a69fc0c',
+        regionCode: 9,
+        scheme: 'sroc',
+        versionNumber: 100,
+        startDate: '2022-04-01',
+        status: 'current',
+        source: 'wrls',
+        changeReasonId: {
+          schema: 'water',
+          table: 'changeReasons',
+          lookup: 'description',
+          value: 'Strategic review of charges (SRoC)',
+          select: 'changeReasonId'
+        }
+      }
+    ],
+    chargeReferences: [
+      {
+        id: 'fa3c73d0-0459-41f0-b6cf-0e0758775ca4',
+        chargeVersionId: '8e5626ee-5e4c-48f6-a668-471d35997e2c',
+        description: 'SROC Charge Reference 01',
+        source: 'tidal',
+        loss: 'medium',
+        factorsOverridden: false,
+        chargeCategoryId: {
+          schema: 'public',
+          table: 'chargeCategories',
+          lookup: 'reference',
+          value: '4.6.12',
+          select: 'id'
+        },
+        adjustments: {
+          aggregate: null,
+          s126: null,
+          s127: true,
+          s130: false,
+          charge: null,
+          winter: false
+        },
+        waterModel: 'no model',
+        volume: 52,
+        eiucRegion: 'Southern',
+        section127Agreement: true
+      },
+      {
+        id: '678997c5-1157-4b80-9800-c32960d05299',
+        chargeVersionId: '8e5626ee-5e4c-48f6-a668-471d35997e2c',
+        description: 'SROC Charge Reference 02',
+        source: 'tidal',
+        loss: 'medium',
+        factorsOverridden: false,
+        chargeCategoryId: {
+          schema: 'public',
+          table: 'chargeCategories',
+          lookup: 'reference',
+          value: '4.6.19',
+          select: 'id'
+        },
+        adjustments: {
+          s126: null,
+          s127: true,
+          s130: false,
+          charge: null,
+          winter: false
+        },
+        waterModel: 'no model',
+        volume: 22,
+        eiucRegion: 'Southern',
+        section127Agreement: true
+      }
+    ],
+    chargeElements: [
+      {
+        id: '0be51375-17b9-40f6-81f5-bd769ba10508',
+        chargeReferenceId: 'fa3c73d0-0459-41f0-b6cf-0e0758775ca4',
+        abstractionPeriodStartDay: 1,
+        abstractionPeriodStartMonth: 4,
+        abstractionPeriodEndDay: 31,
+        abstractionPeriodEndMonth: 3,
+        authorisedAnnualQuantity: 32,
+        section127Agreement: true,
+        description: 'SROC Charge Purpose 01',
+        purposeId: {
+          schema: 'public',
+          table: 'purposes',
+          lookup: 'legacyId',
+          value: '140',
+          select: 'id'
+        },
+        purposePrimaryId: {
+          schema: 'water',
+          table: 'purposesPrimary',
+          lookup: 'legacyId',
+          value: 'A',
+          select: 'purposePrimaryId'
+        },
+        purposeSecondaryId: {
+          schema: 'water',
+          table: 'purposesSecondary',
+          lookup: 'legacyId',
+          value: 'AGR',
+          select: 'purposeSecondaryId'
+        }
+      },
+      {
+        id: '6125a201-6d2e-41f8-85bd-8a5bbf455943',
+        chargeReferenceId: '678997c5-1157-4b80-9800-c32960d05299',
+        abstractionPeriodStartDay: 1,
+        abstractionPeriodStartMonth: 4,
+        abstractionPeriodEndDay: 31,
+        abstractionPeriodEndMonth: 3,
+        authorisedAnnualQuantity: 42,
+        section127Agreement: true,
+        description: 'SROC Charge Purpose 02',
+        purposeId: {
+          schema: 'public',
+          table: 'purposes',
+          lookup: 'legacyId',
+          value: '300',
+          select: 'id'
+        },
+        purposePrimaryId: {
+          schema: 'water',
+          table: 'purposesPrimary',
+          lookup: 'legacyId',
+          value: 'A',
+          select: 'purposePrimaryId'
+        },
+        purposeSecondaryId: {
+          schema: 'water',
+          table: 'purposesSecondary',
+          lookup: 'legacyId',
+          value: 'AGR',
+          select: 'purposeSecondaryId'
+        }
+      }
+    ],
+    returnLogs: [
+      {
+        id: '6a1b2456-e9af-4845-b5a9-a54497dff769',
+        returnReference: '10021668',
+        licenceRef: 'AT/TE/ST/01/01',
+        metadata: {
+          description: 'A DRAIN SOMEWHERE',
+          purposes: [
+            {
+              primary: {
+                code: 'A',
+                description: 'Agriculture'
+              },
+              tertiary: {
+                code: '140',
+                description: 'General Farming & Domestic'
+              },
+              secondary: {
+                code: 'AGR',
+                description: 'General Agriculture'
+              }
+            }
+          ],
+          isTwoPartTariff: true,
+          nald: {
+            periodStartDay: '1',
+            periodStartMonth: '3',
+            periodEndDay: '31',
+            periodEndMonth: '10'
+          }
+        },
+        startDate: '2024-04-01',
+        endDate: '2025-03-21',
+        receivedDate: '2025-03-01',
+        dueDate: '2025-04-28',
+        returnId: 'v1:1:AT/TE/ST/01/01:10021668:2024-04-01:2025-03-31',
+        status: 'due',
+        underQuery: false
+      },
+      {
+        id: '8211606e-302f-4547-9fd0-f3970443ba6d',
+        returnReference: '10021668',
+        licenceRef: 'AT/TE/ST/01/01',
+        metadata: {
+          description: 'A DRAIN SOMEWHERE',
+          purposes: [
+            {
+              primary: {
+                code: 'A',
+                description: 'Agriculture'
+              },
+              tertiary: {
+                code: '300',
+                description: 'Mineral Washing'
+              },
+              secondary: {
+                code: 'AGR',
+                description: 'General Agriculture'
+              }
+            }
+          ],
+          isTwoPartTariff: true,
+          nald: {
+            periodStartDay: '1',
+            periodStartMonth: '3',
+            periodEndDay: '31',
+            periodEndMonth: '10'
+          }
+        },
+        startDate: '2024-10-01',
+        endDate: '2025-03-31',
+        receivedDate: '2025-03-01',
+        dueDate: '2025-04-28',
+        returnId: 'v2:1:AT/TE/ST/01/01:10021668:2024-10-01:2025-03-31',
+        status: 'due',
+        underQuery: false
+      }
+    ]
+  }
+}
