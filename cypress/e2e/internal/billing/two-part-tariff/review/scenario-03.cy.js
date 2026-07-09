d60bdf20d0a44a0db68209c5a648512049acff72
Jason Claxton <jason.claxton@defra.gov.uk>
2026-07-09 11:46:52 +0100

    Update scenario 3 test

--- cypress/e2e/internal/billing/two-part-tariff/review/scenario-03.cy.js
+++ cypress/e2e/internal/billing/two-part-tariff/review/scenario-03.cy.js
@@ -1,17 +1,14 @@
 'use strict'
 
+import scenarioData from '../../../../../support/scenarios/two-part-tariff-review-03.js'
+
+const scenario = scenarioData()
+
 describe('Testing a two-part tariff bill run with a similar licence to scenario one, licence is current and not in workflow, it has one applicable charge version with a single charge reference and one charge element. It has one return however this was received late', () => {
   beforeEach(() => {
     cy.tearDown()
-    // Load the base licence information into the DB
-    cy.fixture('review-scenario-licence.json').then((fixture) => {
-      cy.load(fixture)
-    })
-    // Load the charge and returns information into the DB
-    // NOTE: We set the received date to be after the 'dueDate' so its a late return
-    cy.fixture('review-scenario-03.json').then((fixture) => {
-      cy.load(fixture)
-    })
+
+    cy.load(scenario)
 
     // Get the current date as a string, for example 12 July 2023
     cy.dayMonthYearFormattedDate().then((formattedCurrentDate) => {
