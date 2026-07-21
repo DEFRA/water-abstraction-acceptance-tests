70862c4f9da53aca9a8efe902974b22ade92aaa3
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-07-21 17:52:02 +0100

    Update the licence with charge version scenario
    Because we've split the charge reference from the charge version, we need to update the scenario to include the charge reference data.

--- tests/support/scenarios/unregistered-licence-with-charge-version.scenario.js
+++ tests/support/scenarios/unregistered-licence-with-charge-version.scenario.js
@@ -1,17 +1,18 @@
 import billingAccountData from '../data/billing-account.data.js'
 import chargeVersionData from '../data/charge-version.data.js'
+import chargeReferenceData from '../data/charge-reference.data.js'
 import unregisteredLicenceScenario from './unregistered-licence.scenario.js'
 import { mergeByKey } from '../helpers/scenario.helpers.js'
 
 export const title = 'Unregistered licence with a charge version'
-export const description = 'An unregistered licence, licence holder, company, charge version and billing account'
+export const description = 'Unregistered licence with one charge version based on the licence data'
 
 export default function () {
   const unregisteredLicence = unregisteredLicenceScenario()
 
   const billingAccount = billingAccountData(unregisteredLicence)
-
   const chargeVersion = chargeVersionData(billingAccount, unregisteredLicence)
+  const chargeReference = chargeReferenceData(chargeVersion, unregisteredLicence)
 
-  return mergeByKey(unregisteredLicence, billingAccount, chargeVersion)
+  return mergeByKey(unregisteredLicence, billingAccount, chargeVersion, chargeReference)
 }
