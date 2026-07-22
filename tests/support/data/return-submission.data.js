50b362a310cfb542d615dae2c69554d1210cdc19
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-07-22 09:20:12 +0100

    Prepare return submission for using 'real' values
    Rather than using hard-coded values that don't align with the rest of the licence data, we should look to use values that derive from it.
    
    So, going forward we can assume the scenario will know the overall volume based on either the licence version purpose of the charge reference.
    
    It can pass that value to the return submission data builder and it will apply it across the lines, by using a new `splitTotalVolume()` helper function.
    
    Obviously, the scenario is free to amend the values if it wants. But this gives us a more realistic starting point for the return submission data.

--- tests/support/data/return-submission.data.js
+++ tests/support/data/return-submission.data.js
@@ -1,6 +1,7 @@
+import { splitTotalVolume } from '../helpers/conversion.helpers.js'
 import { generateUUID } from '../helpers/generate-uuid.js'
 
-export default function (period, returnLogData) {
+export default function (period, returnLogData, totalVolume) {
   const {
     returnLogs: [returnLog]
   } = returnLogData
@@ -9,6 +10,8 @@ export default function (period, returnLogData) {
 
   const startYear = period.startDate.getFullYear()
   const endYear = period.endDate.getFullYear()
+
+  const splitVolumes = splitTotalVolume(totalVolume, 12)
 
   return {
     returnSubmissions: [
@@ -26,84 +29,84 @@ export default function (period, returnLogData) {
         returnSubmissionId,
         startDate: `${startYear}-04-01`,
         endDate: `${startYear}-04-30`,
-        quantity: '4000'
+        quantity: splitVolumes[0]
       },
       {
         id: generateUUID(),
         returnSubmissionId,
         startDate: `${startYear}-05-01`,
         endDate: `${startYear}-05-31`,
-        quantity: '4000'
+        quantity: splitVolumes[1]
       },
       {
         id: generateUUID(),
         returnSubmissionId,
         startDate: `${startYear}-06-01`,
         endDate: `${startYear}-06-30`,
-        quantity: '4000'
+        quantity: splitVolumes[2]
       },
       {
         id: generateUUID(),
         returnSubmissionId,
         startDate: `${startYear}-07-01`,
         endDate: `${startYear}-07-31`,
-        quantity: '4000'
+        quantity: splitVolumes[3]
       },
       {
         id: generateUUID(),
         returnSubmissionId,
         startDate: `${startYear}-08-01`,
         endDate: `${startYear}-08-31`,
-        quantity: '4000'
+        quantity: splitVolumes[4]
       },
       {
         id: generateUUID(),
         returnSubmissionId,
         startDate: `${startYear}-09-01`,
         endDate: `${startYear}-09-30`,
-        quantity: '4000'
+        quantity: splitVolumes[5]
       },
       {
         id: generateUUID(),
         returnSubmissionId,
         startDate: `${startYear}-10-01`,
         endDate: `${startYear}-10-31`,
-        quantity: '4000'
+        quantity: splitVolumes[6]
       },
       {
         id: generateUUID(),
         returnSubmissionId,
         startDate: `${startYear}-11-01`,
         endDate: `${startYear}-11-30`,
-        quantity: null
+        quantity: splitVolumes[7]
       },
       {
         id: generateUUID(),
         returnSubmissionId,
         startDate: `${startYear}-12-01`,
         endDate: `${startYear}-12-31`,
-        quantity: null
+        quantity: splitVolumes[8]
       },
       {
         id: generateUUID(),
         returnSubmissionId,
         startDate: `${endYear}-01-01`,
         endDate: `${endYear}-01-31`,
-        quantity: null
+        quantity: splitVolumes[9]
       },
       {
         id: generateUUID(),
         returnSubmissionId,
         startDate: `${endYear}-02-01`,
         endDate: `${endYear}-02-28`,
-        quantity: null
+        quantity: splitVolumes[10]
       },
       {
         id: generateUUID(),
         returnSubmissionId,
         startDate: `${endYear}-03-01`,
         endDate: `${endYear}-03-31`,
-        quantity: '4000'
+        quantity: splitVolumes[11]
       }
     ]
   }
--- tests/support/helpers/conversion.helpers.js
+++ tests/support/helpers/conversion.helpers.js
@@ -22,3 +22,47 @@ export function convertCubicMetresToMegalitres(quantity) {
 
   return Big(quantity).times(0.001).round(MAX_DECIMAL, Big.roundHalfUp).toNumber()
 }
+
+/**
+ * Splits a total volume into an equal number of parts
+ *
+ * We use **Big.js** to mitigate issues with
+ * {@link https://en.wikipedia.org/wiki/Floating-point_error_mitigation | floating-point errors}.
+ *
+ * Each part is rounded to {@link MAX_DECIMAL} decimal places. If rounding causes the sum of all parts to differ from
+ * the original volume, the difference is added to the last part to ensure the total is preserved.
+ *
+ * @param {number} totalVolume - the total volume to be split
+ * @param {number} numberOfSplits - the number of equal parts to split the total volume into
+ *
+ * @returns {number[]} An array of split volumes
+ */
+export function splitTotalVolume(totalVolume, numberOfSplits) {
+  // Work out the individual split volume, rounding to the specified number of decimal places
+  const splitVolume = Big(totalVolume).div(numberOfSplits).round(MAX_DECIMAL, Big.roundHalfUp).toNumber()
+
+  // Make an array of the split volume, equal to the number of splits requested
+  const splits = Array.from({ length: numberOfSplits }, () => {
+    return splitVolume
+  })
+
+  // Sum the splits
+  const sumOfSplits = splits
+    .reduce((acc, val) => {
+      return Big(acc).plus(val)
+    }, Big(0))
+    .toNumber()
+
+  // Check if there is a difference, which can happen due to the rounding
+  const difference = Big(totalVolume).minus(sumOfSplits).toNumber()
+
+  // If there is a difference, we apply it to the last split in the array to ensure their total matches the original
+  // total volume
+  if (difference !== 0) {
+    splits[splits.length - 1] = Big(splits[splits.length - 1])
+      .plus(difference)
+      .toNumber()
+  }
+
+  return splits
+}
