f2c29f3c2676dc5c7d6e0edb69c4ba53b8c65ae1
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-07-27 09:15:22 +0100

    Update charge element data builder
    The first issue is that we don't expect to amend the licence version purpose's (LVP) annual quantity by the amount of elements.
    
    In a simple licence, there will be one LVP. That means the annual quantity for the LVP, will be the total for the licence, and in 99% of cases the total got the charge reference and the charge element.
    
    But we're working towards a licence with two LVPs. This means each will have a quantity, and there total is the total for the licence
    
    - LVP1 100
    - LVP2 200
    - Total 300
    
    Still keeping things simple, the charge version we would expect to see would have one charge reference and two charge elements.
    
    - CHG reference 300
      - CHG element 1 100
      - CHG element 2 200
    
    Each charge element is based on an LVP. But the licence total is assigned to the charge reference. So, we definaely don't want to divide the LVP quantity by the number of elements!
    
    And because each charge element is typically (this is WRLS, so you cannot say always!) based on an LVP, we want to pass it through rather than assume the first LVP is always the one to use.

--- tests/support/data/charge-element.data.js
+++ tests/support/data/charge-element.data.js
@@ -1,14 +1,10 @@
 import { convertCubicMetresToMegalitres } from '../helpers/conversion.helpers.js'
 import { generateUUID } from '../helpers/generate-uuid.js'
 
-export default function (chargeReferenceData, licenceData, noOfElements = 1) {
+export default function (chargeReferenceData, licenceVersionPurpose) {
   const {
     chargeReferences: [chargeReference]
   } = chargeReferenceData
-
-  const {
-    licenceVersionPurposes: [licenceVersionPurpose]
-  } = licenceData
 
   // We make an assumption that if the purpose is not 400 (the default), then we have been passed our alternate which
   // is typically 280 (Make-Up Or Top Up Water). Both have a high loss, and assuming non-tidal and the same volume, both
@@ -25,7 +21,7 @@ export default function (chargeReferenceData, licenceData, noOfElements = 1) {
         abstractionPeriodStartMonth: licenceVersionPurpose.abstractionPeriodStartMonth,
         abstractionPeriodEndDay: licenceVersionPurpose.abstractionPeriodEndDay,
         abstractionPeriodEndMonth: licenceVersionPurpose.abstractionPeriodEndMonth,
-        authorisedAnnualQuantity: convertCubicMetresToMegalitres(licenceVersionPurpose.annualQuantity / noOfElements),
+        authorisedAnnualQuantity: convertCubicMetresToMegalitres(licenceVersionPurpose.annualQuantity),
         loss: 'high',
         section127Agreement: twoPartTariff,
         description,
