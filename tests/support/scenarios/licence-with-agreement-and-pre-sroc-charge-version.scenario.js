15bdd2ec43638841d6154aaab006af402af18e5d
jonathangoulding <goulding.jon@icloud.com>
2026-07-29 16:33:08 +0100

    chore: pre pr checks

--- tests/support/scenarios/licence-with-agreement-and-pre-sroc-charge-version.scenario.js
+++ tests/support/scenarios/licence-with-agreement-and-pre-sroc-charge-version.scenario.js
@@ -2,9 +2,8 @@ import licenceAgreementData from '../data/licence-agreement.data.js'
 import licenceWithPreSrocChargeVersionScenario from './licence-with-pre-sroc-charge-version.scenario.js'
 import { mergeByKey } from '../helpers/scenario.helpers.js'
 
-export const title = 'Licence with an agreement and a pre-SRoC charge version'
-export const description =
-  'A licence, licence holder, company, section 127 two-part tariff agreement, and a charge version and reference pre-dating the SRoC scheme, so it can be used to test old charge scheme behaviour'
+export const title = 'Pre-SRoC Licence with an agreement'
+export const description = 'A pre-Sroc licence, licence holder (company), two-part tariff agreement, and charge version'
 
 export default function () {
   const licence = licenceWithPreSrocChargeVersionScenario()
