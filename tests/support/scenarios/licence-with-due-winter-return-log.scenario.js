0bbbf74b0e917dbdaa66846a33953509db3b2745
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-07-29 14:08:54 +0100

    Fix Playwright tests that have started failing (#325)
    https://eaflood.atlassian.net/browse/WATER-5722
    
    The date has changed, specifically from 28 July to 29 July. So whadda you know, some tests are failing!
    
    > Though that's not the only reason. Skip to the end for that
    
    In this case it's the ad-hoc returns invitation and reminder tests. It was a mistake to use the 'first-period' scenarios because there would come points where that period would not be applicable.
    
    For example, on 28 July the first two periods would be:
    
    - Quarterly 1 April 2026 to 30 June 2026
    - Winter annual 1 April 2025 to 31 March 2026
    
    Now, on 29 July 2026, they're:
    
    - Quarterly 1 July 2026 to 30 September 2026
    - Summer annual 1 November 2025 to 31 October 2026
    
    When the first period was '1 April 2026 to 30 June 2026 ', and we were creating a return log based on that, it would pass because the current date is later than its end date.
    
    Now the first period is '1 July 2026 to 30 September 2026'; the current date is earlier than its end date, so the return log is `NOT DUE YET`.
    
    We should have just used the 'winter-return-log' scenarios. Then we would always have a valid return log that would be picked up for a notification. Only, there is only the 'open-winter-return-log' scenario!
    
    So, the first step is adding a base `licence-with-due-winter-return-log.scenario.js`, which we can then build a `registered-licence-with-due-winter-return-log.scenario.js` on top of.
    
    We then update the ad-returns invite to use the existing `registered-licence-with-open-winter-return-log.scenario.js`, and the ad-hoc returns reminder to use the new `registered-licence-with-due-winter-return-log.scenario.js`.
    
    The other reason is that one of the tests has decided to stop registering a click on an element. It's worked up until now, and the locator is finding the element to click, but it's just stopped failing. Even the AI doesn't know why (and before you say anything, the solution is not to scroll it into view!)
    
    Switching to `getByRole()` does the trick. And because we know the scenario and that it's using a winter return log, we know the first line to 'click' will always be April.

---
+++ tests/support/scenarios/licence-with-due-winter-return-log.scenario.js
@@ -0,0 +1,43 @@
+import returnLogData from '../data/return-log.data.js'
+import returnRequirementData from '../data/return-requirement.data.js'
+import returnVersionData from '../data/return-version.data.js'
+import licenceScenario from './licence.scenario.js'
+import { previousPeriod } from '../helpers/date.helpers.js'
+import { mergeByKey } from '../helpers/scenario.helpers.js'
+
+export const title = 'Licence with an open return log (winter cycle)'
+export const description =
+  'Licence with one return requirement and an open winter return log for the previous winter cycle'
+
+export default function (calculatedDates) {
+  const { currentWinterReturnCycle } = calculatedDates
+
+  const previousPeriodDetails = previousPeriod({
+    startDate: currentWinterReturnCycle.startDate,
+    endDate: currentWinterReturnCycle.endDate,
+    dueDate: currentWinterReturnCycle.dueDate,
+    quarterly: false
+  })
+
+  const currentPeriodDetails = {
+    startDate: new Date(currentWinterReturnCycle.startDate),
+    endDate: new Date(currentWinterReturnCycle.endDate),
+    dueDate: null,
+    quarterly: false
+  }
+
+  const licence = licenceScenario()
+
+  const returnVersion = returnVersionData(licence)
+
+  // In the service return logs will cover the whole period of their matching return version. To ensure our test data is
+  // realistic, we alter the start date of the return version to match the first return log we're seeding.
+  returnVersion.returnVersions[0].startDate = previousPeriodDetails.startDate
+
+  const returnRequirement = returnRequirementData(returnVersion, licence)
+
+  const previousReturnLog = returnLogData(licence, returnRequirement, previousPeriodDetails)
+  const currentReturnLog = returnLogData(licence, returnRequirement, currentPeriodDetails)
+
+  return mergeByKey(licence, returnVersion, returnRequirement, previousReturnLog, currentReturnLog)
+}
