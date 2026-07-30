bc69f6488aad74cbd3fb79c45c8d8d99d1c4206c
jonathangoulding <goulding.jon@icloud.com>
2026-07-30 13:09:18 +0100

    chore: pre pr fix

--- tests/support/scenarios/presroc-licence-with-agreement.scenario.js
+++ tests/support/scenarios/presroc-licence-with-agreement.scenario.js
@@ -1,12 +1,12 @@
 import licenceAgreementData from '../data/licence-agreement.data.js'
-import presrocLicenceWithChargeVersionScenario from './presroc-licence-with-charge-version.scenario.js'
+import presrocLicenceScenario from './presroc-licence.scenario.js'
 import { mergeByKey } from '../helpers/scenario.helpers.js'
 
-export const title = 'Pre-SRoC licence with an agreement'
-export const description = 'A pre-Sroc licence, licence holder (company), two-part tariff agreement, and charge version'
+export const title = 'Presroc licence with an agreement'
+export const description = 'A presroc licence, licence holder (company), two-part tariff agreement'
 
 export default function () {
-  const licence = presrocLicenceWithChargeVersionScenario()
+  const licence = presrocLicenceScenario()
 
   const licenceAgreement = licenceAgreementData(licence)
 
