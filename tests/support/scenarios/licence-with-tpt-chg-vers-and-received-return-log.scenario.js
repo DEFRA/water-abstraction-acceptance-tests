6d245b35e8235d721c5d5e681fd3d9cb4e4dc11a
Jason Claxton <jason.claxton@defra.gov.uk>
2026-08-03 16:33:13 +0100

    Create scenario data

---
+++ tests/support/scenarios/licence-with-tpt-chg-vers-and-received-return-log.scenario.js
@@ -0,0 +1,17 @@
+import licenceWithTptChargeVersionAndCompletedReturnLogScenario from './licence-with-tpt-chg-vers-and-completed-return-log.scenario.js'
+
+export const title = 'Licence with tpt charge version and a return log with a status of "received"'
+export const description =
+  'Licence with a return version and TPT charge version based on the licence data, plus a return log for the previous winter cycle with a status of "received"'
+
+export default function (calculatedDates) {
+  const licence = licenceWithTptChargeVersionAndCompletedReturnLogScenario(calculatedDates)
+
+  const {
+    returnLogs: [returnLog]
+  } = licence
+
+  returnLog.status = 'received'
+
+  return licence
+}
