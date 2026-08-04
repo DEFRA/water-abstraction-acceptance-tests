b7dedae2e66d94ccb2ad6a0b16cbb98bf836e84c
Jason Claxton <jason.claxton@defra.gov.uk>
2026-08-04 14:54:40 +0100

    Create scenario for tpt due return log

---
+++ tests/support/scenarios/licence-with-tpt-chg-vers-and-due-return-log.scenario.js
@@ -0,0 +1,66 @@
+import licenceWithChargeVersionScenario from './licence-with-charge-version.scenario.js'
+import returnLogData from '../data/return-log.data.js'
+import returnRequirementData from '../data/return-requirement.data.js'
+import returnRequirementPointData from '../data/return-requirement-point.data.js'
+import returnRequirementPurposeData from '../data/return-requirement-purpose.data.js'
+import returnVersionData from '../data/return-version.data.js'
+import { previousPeriod } from '../helpers/date.helpers.js'
+
+export const title = 'Licence with tpt charge version and due return log'
+export const description =
+  'Licence with a return version and TPT charge version based on the licence data, plus a due return log for the previous winter cycle'
+
+export default function (calculatedDates) {
+  const { currentWinterReturnCycle } = calculatedDates
+
+  const previousPeriodDetails = previousPeriod({
+    startDate: currentWinterReturnCycle.startDate,
+    endDate: currentWinterReturnCycle.endDate,
+    dueDate: null,
+    quarterly: false
+  })
+
+  const currentPeriodDetails = {
+    startDate: new Date(currentWinterReturnCycle.startDate),
+    endDate: new Date(currentWinterReturnCycle.endDate),
+    dueDate: null,
+    quarterly: false
+  }
+
+  const licence = licenceWithChargeVersionScenario()
+
+  const returnVersion = returnVersionData(licence.licence)
+
+  // In the service return logs will cover the whole period of their matching return version. To ensure our test data is
+  // realistic, we alter the start date of the return version to match the first return log we're seeding.
+  returnVersion.startDate = previousPeriodDetails.startDate
+
+  const returnRequirement = returnRequirementData(returnVersion, licence.licenceVersionPurpose)
+
+  const returnRequirementPoint = returnRequirementPointData(returnRequirement, licence.point)
+  const returnRequirementPurpose = returnRequirementPurposeData(returnRequirement, licence.licenceVersionPurpose)
+
+  const previousReturnLog = returnLogData(
+    licence.licence,
+    returnRequirement,
+    [returnRequirementPurpose],
+    [licence.point],
+    previousPeriodDetails
+  )
+  const currentReturnLog = returnLogData(
+    licence.licence,
+    returnRequirement,
+    [returnRequirementPurpose],
+    [licence.point],
+    currentPeriodDetails
+  )
+
+  return {
+    ...licence,
+    returnVersion,
+    returnRequirement,
+    returnRequirementPoint,
+    returnRequirementPurpose,
+    returnLogs: [previousReturnLog, currentReturnLog]
+  }
+}
