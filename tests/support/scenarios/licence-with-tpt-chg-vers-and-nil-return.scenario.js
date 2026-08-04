c6a5017ab596403d874f3fa7bd2299cee1d1fca0
Jason Claxton <jason.claxton@defra.gov.uk>
2026-08-04 12:29:00 +0100

    Update scenario data to work with new pattern

--- tests/support/scenarios/licence-with-tpt-chg-vers-and-nil-return.scenario.js
+++ tests/support/scenarios/licence-with-tpt-chg-vers-and-nil-return.scenario.js
@@ -1,19 +1,75 @@
-import licenceWithTptChargeVersionAndCompletedReturnLogScenario from './licence-with-tpt-chg-vers-and-completed-return-log.scenario.js'
+import returnLogData from '../data/return-log.data.js'
+import returnRequirementData from '../data/return-requirement.data.js'
+import returnRequirementPointData from '../data/return-requirement-point.data.js'
+import returnRequirementPurposeData from '../data/return-requirement-purpose.data.js'
+import returnSubmissionData from '../data/return-submission.data.js'
+import returnVersionData from '../data/return-version.data.js'
+import licenceWithChargeVersionScenario from './licence-with-charge-version.scenario.js'
+import { previousPeriod } from '../helpers/date.helpers.js'
 
 export const title = 'Licence with tpt charge version and a nil return'
 export const description =
   'Licence with a return version and TPT charge version based on the licence data, plus a completed return log for the previous winter cycle that is a nil return'
 
 export default function (calculatedDates) {
-  const licence = licenceWithTptChargeVersionAndCompletedReturnLogScenario(calculatedDates)
+  const { currentWinterReturnCycle } = calculatedDates
 
-  const {
-    returnSubmissions: [returnSubmission]
-  } = licence
+  const previousPeriodDetails = previousPeriod({
+    startDate: currentWinterReturnCycle.startDate,
+    endDate: currentWinterReturnCycle.endDate,
+    dueDate: null,
+    quarterly: false
+  })
 
+  const currentPeriodDetails = {
+    startDate: new Date(currentWinterReturnCycle.startDate),
+    endDate: new Date(currentWinterReturnCycle.endDate),
+    dueDate: null,
+    quarterly: false
+  }
+
+  const licence = licenceWithChargeVersionScenario()
+
+  const returnVersion = returnVersionData(licence.licence)
+
+  // In the service return logs will cover the whole period of their matching return version. To ensure our test data is
+  // realistic, we alter the start date of the return version to match the first return log we're seeding.
+  returnVersion.startDate = previousPeriodDetails.startDate
+
+  const returnRequirement = returnRequirementData(returnVersion, licence.licenceVersionPurpose)
+
+  returnRequirement.twoPartTariff = true
+
+  const returnRequirementPoint = returnRequirementPointData(returnRequirement, licence.point)
+  const returnRequirementPurpose = returnRequirementPurposeData(returnRequirement, licence.licenceVersionPurpose)
+
+  const previousReturnLog = returnLogData(
+    licence.licence,
+    returnRequirement,
+    [returnRequirementPurpose],
+    [licence.point],
+    previousPeriodDetails
+  )
+  const currentReturnLog = returnLogData(
+    licence.licence,
+    returnRequirement,
+    [returnRequirementPurpose],
+    [licence.point],
+    currentPeriodDetails
+  )
+
+  previousReturnLog.status = 'completed'
+
+  const returnSubmission = returnSubmissionData(previousReturnLog)
   returnSubmission.nilReturn = true
 
-  delete licence.returnSubmissionLines
-
-  return licence
+  return {
+    ...licence,
+    returnVersion,
+    returnRequirement,
+    returnRequirementPoint,
+    returnRequirementPurpose,
+    returnLogs: [previousReturnLog, currentReturnLog],
+    returnSubmission
+  }
 }
