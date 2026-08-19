bc26763fee8909ea062e59bb4c511b78db6e3f98
jonathangoulding <goulding.jon@icloud.com>
2026-08-19 10:55:33 +0100

    chore: pr issues

--- tests/config.js
+++ tests/config.js
@@ -4,7 +4,7 @@ dotenv.config()
 
 export default {
   baseUrl: 'http://localhost:8008',
-  defaultPassword: 'P@55word',
+  defaultPassword: process.env.DEFAULT_USER_PASSWORD,
   externalUrl: 'http://localhost:8000',
   jwtToken: process.env.JWT_TOKEN,
   notifyCallbackToken: process.env.NOTIFY_CALLBACK_TOKEN
