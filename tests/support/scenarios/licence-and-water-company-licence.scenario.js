fd411cfec0e4ceb0311eb64d25161e4ac430f161
jonathangoulding <goulding.jon@icloud.com>
2026-09-08 15:40:54 +0100

    chore: lint issues

--- tests/support/scenarios/licence-and-water-company-licence.scenario.js
+++ tests/support/scenarios/licence-and-water-company-licence.scenario.js
@@ -6,8 +6,8 @@ import {
 import { asArrays } from '../helpers/wire-format.helpers.js'
 import licenceWithChargeVersionScenario from './licence-with-charge-version.scenario.js'
 import { mergeByKey } from '../helpers/scenario.helpers.js'
-import { generateAccountNumber, generatePointExternalId } from '../helpers/generators.helpers.js'
 import { regions } from '../default-values.js'
+import { generateAccountNumber, generatePointExternalId } from '../helpers/generators.helpers.js'
 
 export const title = 'A licence and a water company licence'
 export const description = 'A licence and a water company licence, each with a charge version and billing account'
